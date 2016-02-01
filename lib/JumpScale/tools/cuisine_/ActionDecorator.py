863ddcc2833305a95c88db7fd3edbc50734e0190
Abdulrahman tarek khalifa <tareka@codescalers.com>
2016-02-01 15:31:55 +0200

    adjusted the decorator to take dependencies

--- lib/JumpScale/tools/cuisine_/ActionDecorator.py
+++ lib/JumpScale/tools/cuisine_/ActionDecorator.py
@@ -3,15 +3,16 @@ from JumpScale import j
 
 class ActionDecorator(object):
 
-    def __init__(self,action=False,force=False):
+    def __init__(self,action=False,force=False, deps=[]):
         self.action=action
         self.force=force
-
+        self.deps = deps
     def __call__(self, func):
 
         def wrapper(*args, **kwargs):
             actionbase=self.action
             forcebase=self.force
+            dependencies =self.dependencies
             cm=self.selfobjCode
             cuisine=args[0].cuisine
             if "action" in kwargs:
@@ -27,7 +28,7 @@ class ActionDecorator(object):
                 args=args[1:]
                 cm=cm.replace("$id",cuisine.id)  #replace the code which has the constructor code for the selfobj to work on
                 j.actions.setRunId(cuisine.runid)
-                action0=j.actions.add(action=func,actionRecover=None,args=args,kwargs=kwargs,die=True,stdOutput=True,errorOutput=True,retry=0,executeNow=True,selfGeneratorCode=cm,force=force)
+                action0=j.actions.add(action=func, deps=dependencies, actionRecover=None,args=args,kwargs=kwargs,die=True,stdOutput=True,errorOutput=True,retry=0,executeNow=True,selfGeneratorCode=cm,force=force)
 
                 if action0.state!="OK":
                     if "die" in kwargs:

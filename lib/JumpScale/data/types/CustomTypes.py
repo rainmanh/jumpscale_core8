203fa1d28e06009bbe295188e026cf9e8779b91f
Mohamed Elawadi <mielawadi@gmail.com>
2016-02-09 12:31:15 +0200

    clean extra chars from phone numbers

--- lib/JumpScale/data/types/CustomTypes.py
+++ lib/JumpScale/data/types/CustomTypes.py
@@ -87,20 +87,24 @@ class Path(Email):
 
 class Tel(Email):
     """
-    format is e.g. +32 475.99.99.99
+    format is e.g. +32 475.99.99.99x123
     only requirement is it needs to start with +
     the. & , and spaces will not be remembered
+    and x stands for phone number extension
     """
     def __init__(self):
         Email.__init__(self)
         self.NAME = 'tel'
-        self._RE = re.compile('^\+?[0-9]+(?:\([0-9]+\))?(?:[0-9]+[-. ]?)+(?:x[0-9]+)?$')
+        self._RE = re.compile('^\+?[0-9]{6,15}(?:x[0-9]+)?$')
 
     def clean(self,v):
         if not j.data.types.string.check(v):
             raise ValueError("Input needs to be string:%s"%v)
         v=v.replace(".","")
         v=v.replace(",","")
+        v=v.replace("-","")
+        v=v.replace("(","")
+        v=v.replace(")","")
         v=v.replace(" ","")
         v.strip()
         return v

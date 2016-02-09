6174a81d9a26d7b1951c3e8377cffcb43b326295
christophe de Carvalho Pereira Martincs <christophe.dcpm@gmail.com>
2016-02-09 10:31:02 +0100

    fix call to cuisine.package

--- lib/JumpScale/tools/cuisine_/CuisineInstaller.py
+++ lib/JumpScale/tools/cuisine_/CuisineInstaller.py
@@ -391,10 +391,10 @@ class CuisineInstaller(object):
         self.cuisine.run_script(out)
 
         if not self.cuisine.isMac:
-            self.package.install("fuse")
+            self.cuisine.package.install("fuse")
 
         if self.cuisine.isArch:
-            self.package.install("wpa_actiond") #is for wireless auto start capability
+            self.cuisine.package.install("wpa_actiond") #is for wireless auto start capability
             #systemctl enable netctl-auto@wlan0.service
 
         self.cuisine.package.multiInstall(C)
@@ -681,8 +681,8 @@ class CuisineInstaller(object):
 
         for deviceid,size in devs:
             partition(deviceid,size)
-            
-        
+
+
     @actionrun(action=True)
     def docker(self):
         if self.cuisine.isUbuntu:

70e0a1822acf03d2f992ec85443f1d321ad3355e
christophe de Carvalho Pereira Martincs <christophe.dcpm@gmail.com>
2016-03-09 17:32:55 +0400

    fix user model

--- lib/JumpScale/data/models/Models.py
+++ lib/JumpScale/data/models/Models.py
@@ -82,7 +82,7 @@ class ModelBase():
                 res = None
         else:
             try:
-                res = cls.objects.get(guid=guid)
+                res = cls.objects.get(pk=guid)
             except DoesNotExist:
                 res = None
         return res
@@ -147,7 +147,7 @@ class ModelBase():
 
 class Errorcondition(ModelBase, Document):
     nid = IntField(required=True)
-    gid = IntField(required=True)    
+    gid = IntField(required=True)
     aid = IntField(default=0)
     pid = IntField(default=0)
     jid = StringField(default='')  #@todo (*2*) is this right, string???
@@ -288,7 +288,7 @@ class VDisk(ModelBase, Document):
     backuplocation = StringField()
     backuptime  = IntField(default=j.data.time.getTimeEpoch())
     backupexpiration = IntField()
-    
+
 class Alert(ModelBase, Document):
     username = StringField(default='')
     description = StringField(default='')

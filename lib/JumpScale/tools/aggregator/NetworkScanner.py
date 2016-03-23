d084f574f9e2bd79ef81feee1f41d48222937bd1
Jo De Boeck <deboeck.jo@gmail.com>
2016-03-23 09:12:08 +0200

    NetworkScanner: Use nmap's xml output

--- lib/JumpScale/tools/aggregator/NetworkScanner.py
+++ lib/JumpScale/tools/aggregator/NetworkScanner.py
@@ -1,9 +1,10 @@
 import re
+from xml.etree import ElementTree
 from JumpScale import j
 
 
 class NetworkScanner(object):
-    COMMAND = 'nmap -n --disable-arp-ping -send-ip -Pn -sS -p{ports} -oG - {cidr}'
+    COMMAND = 'nmap -n --disable-arp-ping -send-ip -Pn -sS -p{ports} -oX - {cidr}'
 
     def __init__(self, cidr, ports=[80]):
         code, _ = j.sal.process.execute('which nmap', outputToStdout=False, die=False)
@@ -14,16 +15,18 @@ class NetworkScanner(object):
         self._cidr = cidr
 
     def scan(self):
-        """nmap -n --disable-arp-ping -send-ip -Pn -sS -p22 -oG - 172.17.0.1/24"""
+        """nmap -n --disable-arp-ping -send-ip -Pn -sS -p22 -oX - 172.17.0.1/24"""
 
         cmd = self.COMMAND.format(ports=self._ports, cidr=self._cidr)
         code, output = j.sal.process.execute(cmd, outputToStdout=False, die=False)
         if code != 0:
             raise RuntimeError('nmap scan failed')
 
-        regex = re.compile(r'^Host: ([^\(]+) .+Ports: %s/open' % self._ports, re.MULTILINE)
-        hosts = []
-        for match in regex.finditer(output):
-            hosts.append(match.group(1))
-
+        dom = ElementTree.fromstring(output)
+        hosts = {}
+        for host in dom.findall('host'):
+            address = host.find('address').attrib['addr']
+            for port in host.findall('ports/port'):
+                if port.find('state').attrib['state'] == 'open':
+                    hosts.setdefault(address, []).append(int(port.attrib['portid']))
         return hosts

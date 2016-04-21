f2b304861653e61d4c8f0f6bbdd880cce3591b36
Abdelrahman Hussein <abdelrahman.hussein@hgst.com>
2016-04-21 15:23:12 +0200

    directory structure for unittets

---
+++ test/lib/JumpScale/clients/ssh/SSHClientTest.py
@@ -0,0 +1,14 @@
+"""
+Unit tests module for lib.Jumpscale.clients.ssh.SSHClient module
+"""
+
+import unittest2 as unittest
+import mock
+
+
+class SSHClientTest(unittest.TestCase):
+    """
+    Test SSHClient class
+    """
+
+    

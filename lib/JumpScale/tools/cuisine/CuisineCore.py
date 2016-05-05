4810bda371797dcd034496f470fb06b9659fae38
Maxime Daniel <root@maxux.net>
2016-05-05 10:54:17 +0200

    adding file streaming on cuisine and python execution

--- lib/JumpScale/tools/cuisine/CuisineCore.py
+++ lib/JumpScale/tools/cuisine/CuisineCore.py
@@ -641,7 +641,7 @@ class CuisineCore():
 
     def _file_stream(self, input, output):
         while True:
-            piece = input.read(512 * 1024) # 512 kB chunk
+            piece = input.read(131072)
             if not piece:
                 break
             

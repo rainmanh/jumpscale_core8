bdee199c6923fc03829b622b9b61baae23807618
Abdelrahman Hussein <ahussein.abdelrahman@gmail.com>
2016-05-24 12:58:13 +0200

    Revert "update ssh client test"
    This reverts commit 8ee0a66942ed22324a24f1098467b7a5b0ba3e3c.

--- test/lib/JumpScale/clients/ssh/SSHClientTest.py
+++ test/lib/JumpScale/clients/ssh/SSHClientTest.py
@@ -11,14 +11,4 @@ class SSHClientTest(unittest.TestCase):
     Test SSHClient class
     """
 
-    def test_sshclientfactory_get(self):
-        """
-        Tests getting new client using the client factory
-        """
-        with mock.patch('Jumpscale') as js_mock:
-            from lib.Jumpscale.clients.ssh import SSHClient
-            with mock.patch('j.data.hash.md5_string') as md5_string_mock:
-                md5_string_mock.return_value = 'pass'
-                SSHClient.SSHClient = mock.Mock()
-                sshclient_factory = SSHClient.SSHClientFactory()
-                sshclinet = sshclient_factory.get(addr="localhost")
+    

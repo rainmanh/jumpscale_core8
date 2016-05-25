7e5fad13e5e6da4492edf4b7f77bc5072b43b9d8
abdulrahmantkhalifa <abdulrahman.t.khalifa@gmail.com>
2016-05-25 12:04:19 +0300

    extended gogs client (#222)
    * extended client
    
    now supports issues , also some adjustments were made
    to support organization's repos and thier functions
    
    * cleaned up code

--- lib/JumpScale/clients/gogs/GogsClient.py
+++ lib/JumpScale/clients/gogs/GogsClient.py
@@ -32,15 +32,15 @@ class GogsServerErrorException(GogsBaseException):
 
 class GogsClient:
     """
-    some depend on an edited gogs , which is not yet pushed 
+    some depend on an edited gogs , which is not yet pushed
     """
 
     def __init__(self, addr, port, login, passwd):
-        self.addr = addr
+        self.addr = addr if "http://" in addr else "http://" + addr
         self.port = port
         self._login = login
         self._passwd = passwd
-        self.base_url = "%s:%s/api/v1" % (addr, port)
+        self.base_url = "%s:%s/api/v1" % (self.addr, self.port)
 
         self.session = requests.session()
         self.session.auth = HTTPBasicAuth(login, passwd)
@@ -49,10 +49,12 @@ class GogsClient:
         # modify gogs if have to to get API to do what we want (seems to be
         # incomplete)
 
+    def get_url(self, *args):
+        return j.sal.fs.joinPaths(self.base_url, *args)
+
     def organization_create(self, org_name, full_name=None, user_name=None, description=None, website=None, location=None):
-        pass
         """
-        create an organization by user with name 
+        create an organization by user with name
         """
         if not user_name:
             user_name = self._login
@@ -72,8 +74,8 @@ class GogsClient:
                 '%s/orgs/%s' % (self.base_url, org_name), json=body)
         except NotFoundException:
             response_set = self.session.post(
-                '%s/admin/users/%s/orgs' % (self.base_url, user_name), json=body)
-                                                 # admin/users/abdu/orgs
+                self.get_url("admin", "users", user_name, "orgs"), json=body)
+            # admin/users/abdu/orgs
         if response_set.status_code == 201:
             return response_set.json()
         elif response_set.status_code == 422:
@@ -84,7 +86,7 @@ class GogsClient:
 
     def organization_delete(self, org_name):
         """
-        delete organization 
+        delete organization
         """
         response_delete = self.session.delete(
             '%s/admin/users/%s/orgs/%s' % (self.base_url, self._login, org_name))
@@ -99,12 +101,13 @@ class GogsClient:
         """
         add user to organization
         """
-        body ={
+        body = {
             "username": user_name
         }
         self.user_get(user_name)
 
-        response_add = self.session.post("%s/admin/orgs/%s/users/"%(self.base_url, org_name), json=body)
+        response_add = self.session.post(
+            self.get_url("admin", "orgs", org_name, "users"), json=body)
 
         if response_add.status_code == 201:
             return True
@@ -117,15 +120,14 @@ class GogsClient:
         elif response_add.status_code == 500:
             raise GogsServerErrorException('gogs server error')
 
-
-
     def organizations_user_delete(self, org_name, user_name):
         """
-        remove user from organisation 
+        remove user from organisation
         """
         self.user_get(user_name)
 
-        response_delete = self.session.delete("%s/admin/orgs/%s/users/%s"%(self.base_url, org_name, user_name))
+        response_delete = self.session.delete(
+            self.get_url("admin", "orgs", org_name, "users", user_name))
 
         if response_delete.status_code == 204:
             return True
@@ -138,17 +140,15 @@ class GogsClient:
         elif response_delete.status_code == 500:
             raise GogsServerErrorException('gogs server error')
 
-
-
     def organizations_list(self, user_name=None):
         """
-        list organizations of current user         
+        list organizations of current user
         """
         if not user_name:
-            response_orgs = self.session.get('%s/user/orgs' % (self.base_url))
+            response_orgs = self.session.get(self.get_url("user", "orgs"))
         else:
             response_orgs = self.session.get(
-                '%s/users/%s/orgs' % (self.base_url, user_name))
+                self.get_url("users", user_name, "orgs"))
         if response_orgs.status_code == 200:
             return response_orgs.json()
 
@@ -158,8 +158,7 @@ class GogsClient:
         """
         if not org_name:
             return self.organizations_list()
-        response_org = self.session.get(
-            '%s/orgs/%s' % (self.base_url, org_name))
+        response_org = self.session.get(self.get_url("orgs", org_name))
         if response_org.status_code == 200:
             return response_org.json()
         elif response_org.status_code == 403:
@@ -193,7 +192,7 @@ class GogsClient:
         if pubkey:
             data = {"title": pubkey_name(), "key": pubkey}
             response_pubk = self.session.post(
-                '%s/user/keys/' % (self.base_url), json=data)
+                self.get_url("user", "keys"), json=data)
             if response_pubk.status_code == 422:
                 raise DataErrorException('pubkey exists or is invalid')
 
@@ -203,7 +202,7 @@ class GogsClient:
                 '%s/admin/users/%s' % (self.base_url, name), json=body)
         except NotFoundException:
             response_set = self.session.post(
-                '%s/admin/users' % (self.base_url), json=body)
+                self.get_url("admin", "users"), json=body)
 
         if response_set.status_code == 201:
             return True
@@ -215,11 +214,10 @@ class GogsClient:
 
     def user_delete(self, name):
         """
-        deletes a user with provided username   
+        deletes a user with provided username
         """
-
         response_delete = self.session.delete(
-            '%s/admin/users/%s' % (self.base_url, name))
+            self.get_url("admin", "users", name))
         if response_delete.status_code == 204:
             return True
         elif response_delete.status_code == 403:
@@ -232,12 +230,11 @@ class GogsClient:
             raise GogsServerErrorException('gogs server error')
 
     def users_list(self, page=1, limit=10):
-        pass
         """
         returns a json that lists the users , accesible from admin accounts
         """
         response_list = self.session.get(
-            '%s/admin/users/' % (self.base_url), params={'page': page, "limit": limit})
+            self.get_url("admin", "users"), params={'page': page, "limit": limit})
 
         if response_list.status_code == 200:
             return response_list.json()
@@ -251,7 +248,7 @@ class GogsClient:
         if not name:
             name = self._login
 
-        response_user = self.session.get('%s/users/%s' % (self.base_url, name))
+        response_user = self.session.get(self.get_url("users", name))
         if response_user.status_code == 200:
             return response_user.json()
         else:
@@ -259,7 +256,7 @@ class GogsClient:
 
     def repository_create(self, repo_name, organization=None, user_name=None, description="", private=True, readme=True):
         """
-        create repository logged in  username 
+        create repository logged in  username
         """
         body = {
             "name": repo_name,
@@ -270,16 +267,16 @@ class GogsClient:
         if user_name and organization:
             raise DataErrorException(
                 'user_name and organization are mutually exclusive')
-        if user_name:
-            if organization:
-                response_set = self.session.post(
-                    '%s/admin/users/%s/repos' % (self.base_url, organization), json=body)
-            else:
-                response_set = self.session.post(
-                    '%s/admin/users/%s/repos' % (self.base_url, user_name), json=body)
+
+        if organization:
+            response_set = self.session.post(
+                self.get_url("admin", "users", organization, "repos"), json=body)
+        elif user_name:
+            response_set = self.session.post(
+                self.get_url("admin", "users", user_name, "repos"), json=body)
         else:
             response_set = self.session.post(
-                '%s/user/repos' % (self.base_url), json=body)
+                self.get_url("user", "repos"), json=body)
 
         if response_set.status_code == 200:
             return response_set.json()[0]
@@ -289,12 +286,15 @@ class GogsClient:
         elif response_set.status_code == 403:
             raise AdminRequiredException('Admin access Required')
 
-    def repository_delete(self, organization, name):
+    def repository_delete(self, repo_name, owner=None):
         """
         deletes a repository
         """
+        if not owner:
+            owner = self._login
+
         response_delete = self.session.delete(
-            '%s/repos/%s/%s' % (self.base_url, self._login, name))
+            self.get_url("repos", owner, repo_name))
         if response_delete.status_code == 204:
             return True
         elif response_delete.status_code == 403:
@@ -310,7 +310,7 @@ class GogsClient:
         [[$id,$name,$ssh_url]]
         """
         repos = list()
-        response_repos = self.session.get('%s/user/repos' % (self.base_url))
+        response_repos = self.session.get(self.get_url("user", "repos"))
         if response_repos.status_code == 200:
             for repo in response_repos.json():
                 repos.append([repo['id'], repo['full_name'], repo['ssh_url']])
@@ -318,34 +318,38 @@ class GogsClient:
         else:
             raise GogsBaseException()
 
-    def repository_user_add(self, repo_name, username, access="RW"):
-        pass
+    def repository_user_add(self, repo_name, username, owner=None, access="RW"):
         """
         make sure a user can access the repository
-
+        owner can be user or organization
         """
+        if not owner:
+            owner = self._login
+
         body = {
             "username": username
         }
 
-        self.repository_get(repo_name)  # testing to see if user has access
+        # testing to see if user has access
+        self.repository_get(repo_name, owner)
 
         response_repos = self.session.post(
-            '%s/repos/%s/%s/access' % (self.base_url, self._login, repo_name), json=body)
+            self.get_url("repos", owner, repo_name, "access"), json=body)
         if response_repos.status_code == 200:
             return response_repos.json()
         else:
             raise DataErrorException()
 
-    def repository_get(self, repo_name, user_name=None):
+    def repository_get(self, repo_name, owner=None):
         """
         return detailed info as json is ok (result of api call)
+        owner can be user or organization
         """
-        if not user_name:
-            user_name = self._login
+        if not owner:
+            owner = self._login
 
         response_user = self.session.get(
-            '%s/repos/%s/%s' % (self.base_url, user_name, repo_name))
+            self.get_url("repos", owner, repo_name))
         if response_user.status_code == 200:
             return response_user.json()
         elif response_user.status_code == 403:
@@ -353,12 +357,18 @@ class GogsClient:
         else:
             raise NotFoundException("User or repo does not exist")
 
-    def repository_user_delete(self, repo_name, user_name):
+    def repository_user_delete(self, repo_name, user_name, owner=None):
         """
-        removes user from repo collaborators 
+        removes user from repo collaborators
+        owner can be user or organization
         """
+
+        if not owner:
+            owner = self._login
+
         response_delete = self.session.delete(
-            "%s/repos/%s/%s/access/%s" % (self.base_url, self._login, repo_name, user_name))
+            self.get_url("repos", owner, repo_name, "access", user_name))
+
         if response_delete.status_code == 204:
             return True
         elif response_delete.status_code == 403:
@@ -366,14 +376,89 @@ class GogsClient:
         else:
             raise NotFoundException("User or repo does not exist")
 
-    def issues_list():
-        pass
+    def issues_list(self, repo_name, owner=None):
+        """
+        list issues of repo belonging to owner,
+        owner can be user or organization
+        """
+        if not owner:
+            owner = self._login
 
-    def issue_get():
-        pass
+        response_list = self.session.get(
+            self.get_url("repos", owner, repo_name, "issues"))
 
-    def issue_create():
-        pass
+        if response_list.status_code == 200:
+            return response_list.json()
+        elif response_list.status_code == 403:
+            raise AdminRequiredException("user does not have access to repo")
+        else:
+            raise NotFoundException("User or repo does not exist")
 
-    def issue_delete():
-        pass
+    def issue_get(self, repo_name, index, owner=None):
+        """ 
+        get json representation of issue
+        owner can be user or organization
+        """
+        if type(index) == int:
+            index = str(index)
+
+        if not owner:
+            owner = self._login
+
+        response_get = self.session.get(
+            "%s/repos/%s/%s/issues/%s" % ("repos", owner, repo_name, "issues", index))
+
+        if response_get.status_code == 200:
+            return response_get.json()
+        elif response_get.status_code == 403:
+            raise AdminRequiredException("user does not have access to repo")
+        else:
+            raise NotFoundException("User or repo does not exist")
+
+    def issue_create(self, repo_name, title, owner=None, description=None, assignee=None, milestone=None, labels=None, closed=None):
+        """ 
+        create issue 
+        @milestone  = int
+        @id = int 
+        owner can be user or organization
+        """
+
+        body = {
+            "title": title,
+            "body": description,
+            "assignee": assignee,
+            "milestone": milestone,
+            "labels": labels,
+            "closed": closed
+        }
+
+        response_create = self.session.post(
+            self.get_url("repos", owner, repo_name, "issues"), json=body)
+
+        if response_create.status_code == 201:
+            return response_create.json()
+        elif response_create.status_code == 403:
+            raise AdminRequiredException("user does not have access to repo")
+        else:
+            raise NotFoundException("User or repo does not exist")
+
+    def issue_close(self, repo_name, index, owner=None):
+        """ close issue """
+
+        if type(index) == int:
+            index = str(index)
+
+        if not owner:
+            owner = self._login
+
+        body = {"closed": True}
+
+        response_close = self.session.patch(
+            self.get_url("repos", owner, repo_name, "issues", index), json=body)
+
+        if response_close.status_code == 201:
+            return response_close.json()
+        elif response_close.status_code == 403:
+            raise AdminRequiredException("user does not have access to repo")
+        else:
+            raise NotFoundException("User or repo does not exist")

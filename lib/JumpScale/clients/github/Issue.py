ed1f4a25289f993816a433a24531251a651e87f2
Muhamad Azmy <muhamad.azmy@codescalers.com>
2016-06-13 11:16:23 +0200

    GitHub tools refactor (#303)
    * WIP: refactoring to separate processing logic
    
    Take out all logic related to processing issues
    out of the client.
    
    * Fix a bug + calculate estimate
    
    * Retrieve comments only when needed
    
    * Update docstring
    
    * Rename file
    
    * Apply pull request comments

--- lib/JumpScale/clients/github/Issue.py
+++ lib/JumpScale/clients/github/Issue.py
@@ -1,32 +1,23 @@
+import threading
 from JumpScale import j
 from Base import Base
 from Base import replacelabels
-import re
 from github.GithubObject import NotSet
 from Milestone import RepoMilestone
 
-re_story_name = re.compile('.+\((.+)\)\s*$')
-re_task_estimate = re.compile('.+\[([^\]]+)\]\s*$')
-re_story_estimate = re.compile('^ETA:\s*(.+)\s*$', re.MULTILINE)
 
 class Issue(Base):
-
     def __init__(self, repo, ddict={}, githubObj=None):
         self.logger = j.logger.get('j.clients.github.issue')
         self.repo = repo
         self._ddict = ddict
         self._githubObj = githubObj
+        self._comments = None
         if githubObj is not None:
             self.load()
+
+        self._lock = threading.RLock()
         # self.todo
-
-    @staticmethod
-    def get_story_name(title):
-        m = re_story_name.match(title.strip())
-        if m is None:
-            return None
-
-        return m.group(1)
 
     @property
     def api(self):
@@ -39,87 +30,88 @@ class Issue(Base):
         if self._ddict == {}:
             # no dict yet, fetch from github
             self.load()
+        # we lazy load the comments. so it's only loaded when accesses
+        self._ddict['comments'] = self.comments
         return self._ddict
 
     @property
     def comments(self):
-        return self.ddict["comments"]
+        if self._comments is not None:
+            return self._comments
+
+        with self._lock:
+            if self._comments is None:
+                self.logger.debug("Loading comments for issue: %s" % self.number)
+                self._comments = []
+                for comment in self.api.get_comments():
+                    obj = {}
+                    user = self.repo.client.getUserLogin(githubObj=comment.user)
+                    obj["user"] = user
+                    obj["url"] = comment.url
+                    obj["id"] = comment.id
+                    obj["body"] = comment.body
+                    obj["time"] = j.data.time.any2HRDateTime(
+                        [comment.last_modified, comment.created_at])
+                    self._comments.append(obj)
+        return self._comments
 
     @property
     def guid(self):
-        return self.repo.fullname + "_" + str(self.ddict["number"])
+        return self.repo.fullname + "_" + str(self._ddict["number"])
 
     @property
     def number(self):
-        return int(self.ddict["number"])
+        return int(self._ddict["number"])
 
     @property
     def title(self):
-        return self.ddict["title"]
+        return self._ddict["title"]
 
     @property
     def body(self):
-        return self.ddict["body"]
+        return self._ddict["body"]
 
     @body.setter
     def body(self, val):
-        self.ddict["body"] = val
-        self.api.edit(body=self.ddict['body'])
+        self._ddict["body"] = val
+        self.api.edit(body=self._ddict['body'])
 
     @property
     def time(self):
-        return self.ddict["time"]
+        return self._ddict["time"]
 
     @property
     def url(self):
-        return self.ddict["url"]
+        return self._ddict["url"]
 
     @property
     def assignee(self):
-        return self.ddict["assignee"]
+        return self._ddict["assignee"]
 
     @property
     def labels(self):
         #we return a copy so changing the list doesn't actually change the ddict value
-        return self.ddict["labels"][:]
-
-    @property
-    def task_estimate(self):
-        m = re_task_estimate.match(self.title)
-        if m is not None:
-            return m.group(1).strip()
-        return None
-
-    @property
-    def story_estimate(self):
-        if not len(self.comments):
-            return None, None
-        # find last comment with ETA
-        for last in reversed(self.comments):
-            m = re_story_estimate.search(last['body'])
-            if m is not None:
-                return m.group(1), last['id']
-        return None, None
+        return self._ddict["labels"][:]
 
     @property
     def id(self):
-        return self.ddict["id"]
+        return self._ddict["id"]
 
     @labels.setter
     def labels(self, val):
         # check if all are already in labels, if yes nothing to do
-        if len(val) == len(self.ddict["labels"]):
-            self.ddict["labels"].sort()
+        if len(val) == len(self._ddict["labels"]):
+            self._ddict["labels"].sort()
             val.sort()
-            if val == self.ddict["labels"]:
+            if val == self._ddict["labels"]:
                 return
-        self.ddict["labels"] = val
-        toset = [self.repo.getLabel(item) for item in self.ddict["labels"]]
+        self._ddict["labels"] = val
+        toset = [self.repo.getLabel(item) for item in self._ddict["labels"]]
         self.api.set_labels(*toset)
 
     @property
     def milestone(self):
-        return self.ddict["milestone"]
+        return self._ddict["milestone"]
 
     @property
     def state(self):
@@ -140,7 +132,7 @@ class Issue(Base):
 
     @property
     def isOpen(self):
-        return self.ddict['open'] == True
+        return self._ddict['open'] == True
 
     @property
     def type(self):
@@ -187,20 +179,6 @@ class Issue(Base):
     @process.setter
     def process(self, val):
         return self._setLabels(val, "process")
-
-    @property
-    def tasks(self):
-        """
-        If this issue is a story, list all tasks related to it
-        """
-        if not self.isStory:
-            raise j.exceptions.Input("This issue is not a story")
-
-        tasks = []
-        for issue in self.repo.issues:
-            if issue.title.startswith('%s:' % self.story_name):
-                tasks.append(issue)
-        return tasks
 
     def _setLabels(self, val, category):
         if val is None or val == '':
@@ -259,20 +237,6 @@ class Issue(Base):
             self.api.set_labels(*labels2set)
             labels = newlabels
 
-        comments = [comment for comment in self.api.get_comments()]
-        commentsToSet = []
-        if len(comments) > 0:
-            for comment in comments:
-                obj = {}
-                user = self.repo.client.getUserLogin(githubObj=comment.user)
-                obj["user"] = user
-                obj["url"] = comment.url
-                obj["id"] = comment.id
-                obj["body"] = comment.body
-                obj["time"] = j.data.time.any2HRDateTime(
-                    [comment.last_modified, comment.created_at])
-                commentsToSet.append(obj)
-
         self._ddict["labels"] = labels
         self._ddict["id"] = self.api.id
         self._ddict["url"] = self.api.html_url
@@ -286,8 +250,6 @@ class Issue(Base):
 
         self._ddict["body"] = self.api.body
 
-        self._ddict["comments"] = commentsToSet
-
         self._ddict["time"] = j.data.time.any2HRDateTime(
             [self.api.last_modified, self.api.created_at])
 
@@ -298,96 +260,6 @@ class Issue(Base):
         else:
             ms = RepoMilestone(repo=self.repo, githubObj=self.api.milestone)
             self._ddict["milestone"] = "%s:%s" % (ms.number, ms.title)
-
-    def getMarkdown(self, priotype=True):
-        md = j.data.markdown.getDocument()
-        md.addMDComment1Line("issue:%s" % self.number)
-        md.addMDHeader(4, self.title)
-        if self.body is not None and self.body.strip() != "":
-            md.addMDBlock(self.body)
-
-        md.addMDHeader(5, "meta")
-
-        t = md.addMDTable()
-        h = [self.state, "[%s](%s)" % (self.number, self.url)]
-        rows = []
-        t.addHeader(h)
-        t.addRow(["milestone", self.milestone])
-        t.addRow([self.priority, self.type])
-        t.addRow(["assignee", self.assignee])
-        t.addRow(["time", self.time])
-
-        if self.comments != []:
-            for comment in self.comments:
-                md.addMDHeader(5, "comment")
-                md.addMDBlock(comment["body"])
-                h = [
-                    comment["user"], "[%s](%s)" %
-                    (comment["id"], comment["url"])]
-                t = md.addMDTable()
-                t.addHeader(h)
-                t.addRow(["time", comment["time"]])
-
-        return md
-
-    def _loadMD(self, content):
-        md = j.data.markdown.getDocument(content)
-        title = md.items[0]
-        self._ddict["title"] = title.title
-        body = md.items[1]
-        self._ddict["body"] = body.text
-        table = md.items[2]
-        state, tmp = table.header
-        # first label
-        self._ddict["labels"] = ["state_%s" % state]
-
-        prio, ttype = table.rows[1]
-        self._ddict["labels"].append("priority_%s" % prio)
-        self._ddict["labels"].append("type_%s" % ttype)
-
-        self._ddict["comments"] = []
-
-        for row in table.rows:
-            if row[0] == "milestone":
-                ms = row[1]
-                if ms != ".":
-                    self._ddict["milestone"] = ms
-            elif row[0] == "assignee":
-                assignee = row[1]
-                if assignee != ".":
-                    self._ddict["assignee"] = assignee
-            elif row[0] == "time":
-                ttime = row[1]
-                if ttime != ".":
-                    self._ddict["time"] = ttime
-
-        for i in range(3):
-            md.items.pop(0)
-
-        state = "title"
-        comment = {}
-        for item in md.items:
-            if state == "title":
-                comment = {}
-                state = "body"
-                continue
-            if state == "body":
-                comment["body"] = item.text
-                state = "table"
-                continue
-            if state == "table":
-                assignee = item.header[0]
-                url = item.header[1]
-                ttime = item.rows[0][1]
-                id = url.split("]", 1)[0].strip("[")
-                comment["id"] = int(id)
-                url = url.split("]", 1)[1].strip("()")
-                comment["url"] = url
-                comment["time"] = ttime
-                state = "title"
-                # now add the comment
-                self._ddict["comments"].append(comment)
-                continue
 
     @property
     def todo(self):
@@ -416,26 +288,6 @@ class Issue(Base):
             return True
         return False
 
-    @property
-    def story_name(self):
-        name = Issue.get_story_name(self.title)
-        if name is None:
-            return ''
-        return name
-
-    @property
-    def tasks(self):
-        """
-        Only works for issue that is a story.
-        returns the tasks linked to this story.
-        """
-        tasks = []
-        if self.isStory:
-            for issue in self.repo.tasks:
-                if issue.title.startswith(self.story_name):
-                    tasks.append(issue)
-        return tasks
-
     def move_to_repo(self, repo):
         self.logger.info("%s: move to repo:%s" % (self, repo))
         body = "Issue moved from %s\n\n" % self.api.url
@@ -463,104 +315,6 @@ class Issue(Base):
             out += comment.body + "\n\n"
             out += "---\n\n"
         return out
-
-    def add_task(self, task):
-        """
-        If this issue is a story, add a link to a subtasks
-        """
-        if self.repo.api.id != task.repo.api.id:
-            j.exceptions.Input("The task and the story have to be in the same Repository.")
-            return
-
-        if not self.isStory:
-            j.exceptions.Input("This issue is not a story")
-            return
-
-        def state(s):
-            s = s.lower()
-            if s == 'verification':
-                return ':white_circle: Verification'
-            elif s == 'inprogress':
-                return ':large_blue_circle: In Progress'
-            elif s == 'closed':
-                return ':white_check_mark: Closed'
-            else:
-                return ':red_circle: Open'
-
-        task_table_found = False
-        change = False
-        doc = j.data.markdown.getDocument(self.body)
-        i = 0
-
-        for item in doc.items:
-            if change is True:
-                return
-            if item.type == 'table':
-                task_table_found = True
-                table = item
-                existing_tasks = [r[2] for r in table.rows]
-                # add task is not in table yet
-                if not "#%d" % task.number in existing_tasks:
-                    self.logger.info("%s: add task:%s" % (self, task))
-                    change = True
-                    table.addRow([state(task.state), task.title, "#%s" % task.number])
-                    break
-                else:
-                    # update task status if needed
-                    for row in table.rows:
-                        current_state = state('open') if task.isOpen else state('closed')
-                        if row[2] == '#%s' % task.number and row[0] != current_state:
-                            self.logger.info("%s: update task:%s" % (self, task))
-                            change = True
-                            row[0] = current_state
-                            row[1] = task.title
-                            break
-
-        if not task_table_found:
-            change = True
-            table = doc.addMDTable()
-            table.addHeader(['status', 'title', 'link'])
-            table.addRow([state(task.state), task.title, "#%s" % task.number])
-
-        if change:
-            self.ddict["body"] = str(doc)
-            self.api.edit(body=str(doc))
-
-    def link_to_story(self, story):
-        """
-        If this issue is a task from a story, add link in to the story in the description
-        """
-        if self.repo.api.id != story.repo.api.id:
-            raise j.exceptions.Input("The task (%s) and the story (%s) have to be in the same Repository." % (self.title, story.task))
-            # return
-
-        body = self.body
-        if body is None:
-            body = ''
-
-        doc = j.data.markdown.getDocument(body)
-
-        change = False
-        story_line_found = False
-        for item in doc.items:
-            if item.type == 'header' and item.level == 3 and item.title.find("Part of Story") != -1:
-                story_line_found = True
-                story_number = item.title.lstrip('Part of Story: ')
-                if story_number != '#%d' % story.number:
-                    item.title = 'Part of Story: #%s' % story.number
-                    change = True
-                break
-
-        if not story_line_found:
-            change = True
-            doc.addMDHeader(3, 'Part of Story: #%s' % story.number)
-            # make sure it's on the first line of the comment
-            title = doc.items.pop(-1)
-            doc.items.insert(0, title)
-
-        if change:
-            self.logger.info("%s: link to story:%s" % (self, story))
-            self.body = str(doc)
 
     def __str__(self):
         return "issue:%s" % self.title

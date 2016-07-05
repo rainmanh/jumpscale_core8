b0958a30b611a7e414e0b1e4e379041703fc25bc
Reem Khamis <khamisr@codescalers.com>
2016-07-05 12:17:58 +0200

    Fixes #394

--- lib/JumpScale/baselib/atyourservice/AtYourServiceRepo.py
+++ lib/JumpScale/baselib/atyourservice/AtYourServiceRepo.py
@@ -146,7 +146,8 @@ class AtYourServiceRepo():
 
         # load local templates
         path = j.sal.fs.joinPaths(self.basepath, "servicetemplates/")
-        load(self.name, path)
+        if j.sal.fs.exists(path):
+            load(self.name, path)
 
         return self._templates
 

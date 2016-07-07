769a39cde7a63ef320c22f2d7753d4e8d02fbbb1
christophe de Carvalho Pereira Martincs <christophe.dcpm@gmail.com>
2016-07-07 17:06:08 +0200

    copy result of build to bin dir

--- lib/JumpScale/tools/cuisine/apps/CuisineAlba.py
+++ lib/JumpScale/tools/cuisine/apps/CuisineAlba.py
@@ -24,7 +24,7 @@ class Alba:
         self.executor = executor
         self.cuisine = cuisine
         self.logger = j.logger.get("j.tools.cuisine.alba")
-        
+
         self.ocaml_version = '4.02.3'
         self.opam_root = None
 
@@ -44,27 +44,27 @@ class Alba:
         sudo libtool libboost-all-dev fuse sysstat ncurses-dev librdmacm-dev
         """
         self.cuisine.package.multiInstall(apt_deps, allow_unauthenticated=True)
-        
+
         # opam
-        
+
         self.opam_root = self.cuisine.core.args_replace('$tmpDir/OPAM')
-        
+
         # self.cuisine.core.run('wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh')
         self.cuisine.core.file_download('https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh', to='$tmpDir/opam_installer.sh')
         self.cuisine.core.run('bash $tmpDir/opam_installer.sh $binDir %s' % self.ocaml_version, profile=True)
-        
+
         cmd = 'opam init --root=%s --comp %s -a --dot-profile %s' % (self.opam_root, self.ocaml_version, self.cuisine.bash.profilePath)
         self.cuisine.core.run(cmd, profile=True)
-        
+
         cmd = "opam config env --root=%s --dot-profile %s > $tmpDir/opam.env" % (self.opam_root, self.cuisine.bash.profilePath)
         self.cuisine.core.run(cmd, die=False, profile=True)
-        
+
         opam_deps = """ocamlfind ssl.0.5.2 camlbz2 snappy sexplib bisect lwt.2.5.1 camltc \
         cstruct ctypes ctypes-foreign uuidm zarith mirage-no-xen.1 quickcheck.1.0.2 \
         cmdliner conf-libev depext kinetic-client tiny_json ppx_deriving.3.1 \
         ppx_deriving_yojson core.113.00.00 redis uri.1.9.1 result ordma
         """
-        
+
         self.cuisine.core.run_script('source $tmpDir/opam.env && opam update && opam install -y %s' % opam_deps, profile=True)
 
     def _install_deps_intel_storage(self):
@@ -74,7 +74,7 @@ class Alba:
         self.cuisine.core.run('cd $tmpDir && tar xfzv isa-l-2.14.0.tar.gz')
         self.cuisine.core.run('cd $tmpDir/isa-l-2.14.0 && ./autogen.sh && ./configure')
         self.cuisine.core.run('cd $tmpDir/isa-l-2.14.0 && make && make install')
-        
+
         """
         RUN wget https://01.org/sites/default/files/downloads/intelr-storage-acceleration-library-open-source-version/isa-l-2.14.0.tar.gz
         RUN tar xfzv isa-l-2.14.0.tar.gz
@@ -87,7 +87,7 @@ class Alba:
     def _install_deps_cpp(self):
         self.cuisine.package.multiInstall("libgtest-dev cmake", allow_unauthenticated=True)
         self.cuisine.core.run('cd /usr/src/gtest && cmake . && make && mv libg* /usr/lib/')
-        
+
         """
         RUN apt-get update && apt-get -y install libgtest-dev cmake
         RUN cd /usr/src/gtest \
@@ -99,18 +99,19 @@ class Alba:
         return
 
     def _install_deps_arakoon(self):
-        aradest = self.cuisine.git.pullRepo('https://github.com/openvstorage/arakoon.git', depth=None)
+        aradest = self.cuisine.git.pullRepo('https://github.com/openvstorage/arakoon.git', depth=None, ssh=False)
         pfx = 'cd %s && source $tmpDir/opam.env' % aradest
-        
+
         self.cuisine.core.run('%s && git pull && git checkout tags/1.9.3' % pfx)
         self.cuisine.core.run('%s && make' % pfx)
-        
+
         prefix = '%s/%s' % (self.opam_root, self.ocaml_version)
         libdir = 'ocamlfind printconf destdir'
         cmd = '%s && export PREFIX=%s && export OCAML_LIBDIR=`%s` && make install' % (pfx, prefix, libdir)
-        
+
         self.cuisine.core.run_script(cmd, profile=True)
-        
+        self.cuisine.core.file_copy(j.sal.fs.joinPaths(aradest, 'arakoon.native'), "$binDir/arakoon")
+
         """
         RUN git clone https://github.com/openvstorage/arakoon.git
         RUN cd arakoon && git pull && git checkout tags/1.9.3
@@ -124,12 +125,12 @@ class Alba:
 
     def _install_deps_orocksdb(self):
         commit = '8bc61d8a451a2724399247abf76643aa7b2a07e9'
-        orodest = self.cuisine.git.pullRepo('https://github.com/domsj/orocksdb.git', depth=None)
+        orodest = self.cuisine.git.pullRepo('https://github.com/domsj/orocksdb.git', depth=None, ssh=False)
         pfx = 'cd %s && source $tmpDir/opam.env' % orodest
 
         self.cuisine.core.run('%s && git pull && git checkout %s' % (pfx, commit))
         self.cuisine.core.run('%s && ./install_rocksdb.sh && make build install' % pfx)
-        
+
         """
         RUN git clone https://github.com/domsj/orocksdb.git \
             && eval `${opam_env}` \
@@ -138,7 +139,7 @@ class Alba:
             && ./install_rocksdb.sh \
             && make build install
         """
-        
+
         return
 
     def _install_deps_etcd(self):
@@ -148,16 +149,16 @@ class Alba:
         self.cuisine.core.run('cd $tmpDir && tar xfzv etcd-v2.2.4-linux-amd64.tar.gz')
         self.cuisine.core.run('cp $tmpDir/etcd-v2.2.4-linux-amd64/etcd /usr/bin')
         self.cuisine.core.run('cp $tmpDir/etcd-v2.2.4-linux-amd64/etcdctl /usr/bin')
-        
+
         """
         RUN curl -L  https://github.com/coreos/etcd/releases/download/v2.2.4/etcd-v2.2.4-linux-amd64.tar.gz -o etcd-v2.2.4-linux-amd64.tar.gz
         RUN tar xzvf etcd-v2.2.4-linux-amd64.tar.gz
         RUN cp ./etcd-v2.2.4-linux-amd64/etcd /usr/bin \
             && cp ./etcd-v2.2.4-linux-amd64/etcdctl /usr/bin
         """
-        
+
         return
-        
+
     def _install_deps(self):
         self._install_deps_opam()
         self._install_deps_intel_storage()
@@ -166,8 +167,11 @@ class Alba:
         self._install_deps_orocksdb()
         self._install_deps_etcd()
 
-    def _build(self, version='6.0.0'):
-        repo = self.cuisine.git.pullRepo('https://github.com/openvstorage/alba', depth=None)
+    def _build(self):
+        repo = self.cuisine.git.pullRepo('https://github.com/openvstorage/alba', depth=None, ssh=False)
+        self.cuisine.core.run('cd %s; git checkout ubuntu-16.04' % repo)
         self.cuisine.core.run_script('source $tmpDir/opam.env && cd %s; make' % repo, profile=True)
-        
-        
+        self.cuisine.core.file_copy('%s/ocaml/alba.native' % repo, '$binDir/alba')
+        self.cuisine.core.file_copy('%s/ocaml/albamgr_plugin.cmxs' % repo, '$binDir/albamgr_plugin.cmxs')
+        self.cuisine.core.file_copy('%s/ocaml/nsm_host_plugin.cmxs' % repo, '$binDir/nsm_host_plugin.cmxs')
+        self.cuisine.core.file_copy('%s/ocaml/disk_failure_tests.native' % repo, '$binDir/disk_failure_tests.native')

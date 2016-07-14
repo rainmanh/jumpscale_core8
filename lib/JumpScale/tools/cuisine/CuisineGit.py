f0726273d9badbd36f7c763638ccc736997c7074
kristof de spiegeleer <kristof@incubaid.com>
2016-07-14 07:22:54 +0200

    fix cuisine

--- lib/JumpScale/tools/cuisine/CuisineGit.py
+++ lib/JumpScale/tools/cuisine/CuisineGit.py
@@ -18,8 +18,8 @@ class CuisineGit:
 
         self.cuisine.core.dir_ensure(dest,force=False)
         keys = self.cuisine.core.run("ssh-keyscan -H github.com",force=False)
-        self.cuisine.core.file_append("$homeDir/.ssh/known_hosts", keys,force=False)
-        self.cuisine.core.file_attribs("$homeDir/.ssh/known_hosts", mode=600,force=False)
+        self.cuisine.core.file_append("$homeDir/.ssh/known_hosts", keys)
+        self.cuisine.core.file_attribs("$homeDir/.ssh/known_hosts", mode=600)
 
         return j.do.pullGitRepo(url=url,dest=dest,login=login,passwd=passwd,depth=depth,\
             ignorelocalchanges=ignorelocalchanges,reset=reset,branch=branch,revision=revision, ssh=ssh,executor=self.executor)

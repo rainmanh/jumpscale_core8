3554a3dc92148b060fb6e93541f6caede9b91160
Abdelrahman Hussein <ahussein.abdelrahman@gmail.com>
2016-07-22 15:37:47 +0200

    update script

--- tools/monitor_js7_services.py
+++ tools/monitor_js7_services.py
@@ -4,25 +4,64 @@ Python wrapper script to connect a current G8 installation and monitor its js7 s
 """
 
 from JumpScale import j
+import os
 import requests
 
 # parameters that should be in a configuration file
-machine_ip = '192.168.103.168'
+machine_ip = '192.168.103.154'
 machine_login = 'cloudscalers'
-machine_password = '9TUh9n0jG'
-base_path = '/opt/code/github/0-complexity/du-conv-3'
+machine_password = 'Ru1ek03xE'
+base_path = '/opt/code/github/gig-projects/env_du-conv-3/'
+remote_tmp_dir_path = '/optvar/tmp/%s' % machine_login
 nodes = ['du-conv-3-01']
+
+
+def check_cmd_result(exit_code, cmd):
+    """
+    Check if a command exit code is zero if not, raise an exception
+
+    @param exit_code: exit code returned by the command executor
+    @type exit_code: int
+
+    @param cmd: Command that was executed
+    @type cmd: str
+    """
+    if exit_code:
+        raise j.exceptions.RuntimeError('Failed to execute command %s' % cmd)
+
 
 result = {}
 node_status = {'domain': '', 'name': '', 'instance': '', 'priority': '', 'status': '', 'ports': []}
 executor=j.tools.executor.getSSHBased(addr=machine_ip, port=22,login=machine_login,passwd=machine_password)
 cuisine=j.tools.cuisine.get(executor)
 
-cmd = "cd %s;echo %s | sudo -S ays execute -tt node.ssh -tn %%s --cmd 'ays status'"
+create_tmp_dir_cmd = 'echo %s | sudo -S mkdir %s'
+cmd = create_tmp_dir_cmd % (machine_password, remote_tmp_dir_path)
+exit_code, output = cuisine.executor.execute(cmd, die=True, checkok=None, async=False, showout=True, combinestdr=False, timeout=0, env={})
+check_cmd_result(exit_code, cmd)
 
+create_tmp_dir_cmd = 'echo %s | sudo -S chown %s:%s %s'
+cmd = create_tmp_dir_cmd % (machine_password, machine_login, machine_login, remote_tmp_dir_path)
+exit_code, output = cuisine.executor.execute(cmd, die=True, checkok=None, async=False, showout=True, combinestdr=False, timeout=0, env={})
+check_cmd_result(exit_code, cmd)
+
+
+cmd = "cd %s;echo %s | sudo -S ays execute -tt node.ssh -tn %%s --cmd 'ays status'"
 cmd = cmd % (base_path, machine_password)
 cmd = cmd % (nodes[0])
 exit_code, output = cuisine.executor.execute(cmd, die=True, checkok=None, async=False, showout=True, combinestdr=False, timeout=0, env={})
+check_cmd_result(exit_code, cmd)
+
+get_remote_nodes_script = """
+from JumpScale import j
+import os
+
+base_path = '%s'
+os.chdir(base_path)
+print j.clients.openvcloud.get().getRemoteNodes()
+""" % base_path
+
+get_remote_nodes_script_file_name = 'get_remote_nodes_%s.py' % j.data.idgenerator.generateRandomInt(1,10000)
 
 def parse_services_output(data):
     """
@@ -49,4 +88,6 @@ def parse_services_output(data):
 **OK
     """
 
-    
+
+client =  j.clients.openvcloud.get()
+client.getRemoteNodes()

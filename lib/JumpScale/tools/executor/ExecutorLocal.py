fd292b798d3fca605e36a70bf71871900fae9dd2
Reem Khamis <khamisr@codescalers.com>
2016-08-23 17:03:58 +0200

    local executor to always use the j.do executor

--- lib/JumpScale/tools/executor/ExecutorLocal.py
+++ lib/JumpScale/tools/executor/ExecutorLocal.py
@@ -15,15 +15,9 @@ class ExecutorLocal(ExecutorBase):
     def execute(self, cmds, die=True, checkok=None, async=False, showout=True, outputStderr=False, timeout=0, env={}):
         if env:
             self.env.update(env)
-
-        if cmds.find('\n') == -1:
-            if self.debug:
-                print("EXECUTOR:%s" % cmds)
-            return j.do.execute(cmds, die=die, async=async, showout=showout, outputStderr=outputStderr, timeout=timeout)
         if self.debug:
-            print("EXECUTOR:\n%s\n" % cmds)
-
-        return j.sal.process.execute(command=cmds, die=die)
+            print("EXECUTOR:%s" % cmds)
+        return j.do.execute(cmds, die=die, async=async, showout=showout, outputStderr=outputStderr, timeout=timeout)
 
     def executeInteractive(self, cmds, die=True, checkok=None):
         cmds = self._transformCmds(cmds, die, checkok=checkok)

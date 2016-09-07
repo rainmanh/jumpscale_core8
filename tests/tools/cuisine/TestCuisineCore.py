9b789fc5b27320f91acbf8e27ac81e3d506b1cb3
Khaled Karam <mohamedk@greenitglobe.com>
2016-09-07 10:44:50 +0200

    complete testing core.dir_paths method

--- tests/tools/cuisine/TestCuisineCore.py
+++ tests/tools/cuisine/TestCuisineCore.py
@@ -3,22 +3,24 @@ Test CuisineCore module
 """
 import unittest
 from unittest import mock
+import copy
+
 from JumpScale import j
-import fakeredis
+
 
 class TestCuisineCore(unittest.TestCase):
     def setUp(self):
         self.dump_env = {
             'HOME': '/root',
             'HOSTNAME': 'js8-core',
-            'JSBASE': 'js test path',
+            'JSBASE': '/js/path',
             'LD_LIBRARY_PATH': '/opt/jumpscale8//bin',
             'LESSCLOSE': '/usr/bin/lesspipe %s %s',
             'LESSOPEN': '| /usr/bin/lesspipe %s',
             'LS_COLORS': 'rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:',
-            'PATH': '/opt/jumpscale8//bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+            'PATH': '/opt/jumpscale8/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
             'PWD': '/opt/code/github/jumpscale/jumpscale_core8',
-            'PYTHONPATH': '.:/opt/jumpscale8//lib:/opt/jumpscale8//lib/lib-dynload/:/opt/jumpscale8//bin:/opt/jumpscale8//lib/python.zip:/opt/jumpscale8//lib/plat-x86_64-linux-gnu:',
+            'PYTHONPATH': '.:/opt/jumpscale8/lib:/opt/jumpscale8/lib/lib-dynload/:/opt/jumpscale8//bin:/opt/jumpscale8//lib/python.zip:/opt/jumpscale8//lib/plat-x86_64-linux-gnu:',
             'PYTHONUNBUFFERED': '1',
             'SHLVL': '2',
             'TERM': 'xterm',
@@ -29,6 +31,23 @@ class TestCuisineCore(unittest.TestCase):
             '_OLD_PYTHONPATH': ''
         }
         self.core = j.tools.cuisine.local.core
+        self.dir_paths = {'appDir': '/js/path/apps',
+                          'base': '/js/path',
+                          'binDir': '/js/path/bin',
+                          'cfgDir': '/optvar//cfg',
+                          'codeDir': '/opt/code/',
+                          'goDir': '/optvar/go/',
+                          'homeDir': '/root',
+                          'hrdDir': '/optvar//hrd',
+                          'jsLibDir': '/js/path/lib/JumpScale/',
+                          'libDir': '/js/path/lib/',
+                          'logDir': '/optvar//log',
+                          'optDir': '/opt/',
+                          'pidDir': '/optvar//pid',
+                          'tmpDir': '/optvar//tmp',
+                          'tmplsDir': '/js/path/templates',
+                          'varDir': '/optvar/'
+                         }
 
     def tearDown(self):
         pass
@@ -38,7 +57,7 @@ class TestCuisineCore(unittest.TestCase):
         Test accessing the isJS8Sandbox property
         """
         self.assertIsNotNone(self.core.isJS8Sandbox)
-
+    
     @mock.patch('JumpScale.j.tools.cuisine.local.core.getenv')
     @mock.patch('JumpScale.j.core.db')
     def test_dir_paths_property_if_JSBASE_and_linux(self, cache_mock, getenv_mock):
@@ -49,11 +68,122 @@ class TestCuisineCore(unittest.TestCase):
         cache_mock.set.return_value = None
         getenv_mock.return_value = self.dump_env
         result = self.core.dir_paths
-        self.assertEqual(result['base'], self.dump_env['JSBASE'])
-        self.assertEqual(result['codeDir'], '/opt/code/')
-        self.assertEqual(result['appDir'], '{}/apps'.format(self.dump_env['JSBASE']))
-        self.assertEqual(result['tmplsDir'], '{}/templates'.format(self.dump_env['JSBASE']))
-        # self.assertEqual(result['varDir'], '/optvar/')
+        self.assertEqual(result, self.dir_paths)
+    
+    @mock.patch('JumpScale.j.tools.cuisine.local.core.getenv')
+    @mock.patch('JumpScale.j.core.db')
+    def test_dir_paths_property_if_linux(self, cache_mock, getenv_mock):
+        """
+        Happy Path: Test accessing the dir_paths property if JSBASE not found in env
+        """
+        cache_mock.hget.return_value = None
+        cache_mock.set.return_value = None
+
+        # remove JSBASE from dump_env
+        dump_env = copy.deepcopy(self.dump_env)
+        del dump_env['JSBASE']
+        getenv_mock.return_value = dump_env
+
+        expected_result = {
+            'appDir': '/opt/jumpscale8//apps',
+            'base': '/opt/jumpscale8/',
+            'binDir': '/opt/jumpscale8//bin',
+            'cfgDir': '/optvar//cfg',
+            'codeDir': '/opt/code/',
+            'goDir': '/optvar/go/',
+            'homeDir': '/root',
+            'hrdDir': '/optvar//hrd',
+            'jsLibDir': '/opt/jumpscale8//lib/JumpScale/',
+            'libDir': '/opt/jumpscale8//lib/',
+            'logDir': '/optvar//log',
+            'optDir': '/opt/',
+            'pidDir': '/optvar//pid',
+            'tmpDir': '/optvar//tmp',
+            'tmplsDir': '/opt/jumpscale8//templates',
+            'varDir': '/optvar/'
+        }
+        result = self.core.dir_paths
+        self.assertEqual(result, expected_result)
+
+    @mock.patch('JumpScale.j.tools.cuisine.local.core.getenv')
+    @mock.patch('JumpScale.j.core.db')
+    def test_dir_paths_property_if_not_linux(self, cache_mock, getenv_mock):
+        """
+        Happy Path: Test accessing the dir_paths property if JSBASE not found in env and not linux
+        """
+        cache_mock.hget.return_value = None
+        cache_mock.set.return_value = None
+
+        # remove JSBASE from dump_env
+        dump_env = copy.deepcopy(self.dump_env)
+        del dump_env['JSBASE']
+        getenv_mock.return_value = dump_env
+
+        # Mocking isMac property to return True
+        is_mac_original = self.core.isMac
+        type(self.core).isMac = mock.PropertyMock(return_value=True)
+
+        expected_result = {
+            'appDir': '/root/opt/jumpscale8//apps',
+            'base': '/root/opt/jumpscale8/',
+            'binDir': '/root/opt/jumpscale8//bin',
+            'cfgDir': '/root/optvar//cfg',
+            'codeDir': '/root/opt/code/',
+            'goDir': '/root/optvar/go/',
+            'homeDir': '/root',
+            'hrdDir': '/root/optvar//hrd',
+            'jsLibDir': '/root/opt/jumpscale8//lib/JumpScale/',
+            'libDir': '/root/opt/jumpscale8//lib/',
+            'logDir': '/root/optvar//log',
+            'optDir': '/root/opt/',
+            'pidDir': '/root/optvar//pid',
+            'tmpDir': '/root/optvar//tmp',
+            'tmplsDir': '/root/opt/jumpscale8//templates',
+            'varDir': '/root/optvar/'
+        }
+        result = self.core.dir_paths
+        self.assertEqual(result, expected_result)
+        # Reset property mock
+        type(self.core).isMac = is_mac_original
+
+
+    @mock.patch('JumpScale.j.tools.cuisine.local.core.getenv')
+    @mock.patch('JumpScale.j.core.db')
+    def test_dir_paths_property_if_JSBASE_and_not_linux(self, cache_mock, getenv_mock):
+        """
+        Happy Path: Test accessing the dir_paths property if JSBASE in env and not linux
+        """
+        cache_mock.hget.return_value = None
+        cache_mock.set.return_value = None
+
+        getenv_mock.return_value = self.dump_env
+
+        # Mocking isMac property to return True
+        is_mac_original = self.core.isMac
+        type(self.core).isMac = mock.PropertyMock(return_value=True)
+
+        expected_result = {
+            'appDir': '/js/path/apps',
+            'base': '/js/path',
+            'binDir': '/js/path/bin',
+            'cfgDir': '/root/optvar//cfg',
+            'codeDir': '/root/opt/code/',
+            'goDir': '/root/optvar/go/',
+            'homeDir': '/root',
+            'hrdDir': '/root/optvar//hrd',
+            'jsLibDir': '/js/path/lib/JumpScale/',
+            'libDir': '/js/path/lib/',
+            'logDir': '/root/optvar//log',
+            'optDir': '/root/opt/',
+            'pidDir': '/root/optvar//pid',
+            'tmpDir': '/root/optvar//tmp',
+            'tmplsDir': '/js/path/templates',
+            'varDir': '/root/optvar/'
+        }
+        result = self.core.dir_paths
+        self.assertEqual(result, expected_result)
+        # Reset property mock
+        type(self.core).isMac = is_mac_original
 
 
     @unittest.skip("Needs fixing")

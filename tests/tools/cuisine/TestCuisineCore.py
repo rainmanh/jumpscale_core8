acd080c9d697308370cc3aa48dc896d60bccd559
Khaled Karam <mohamedk@greenitglobe.com>
2016-09-14 13:11:10 +0200

    refactor mocking cache

--- tests/tools/cuisine/TestCuisineCore.py
+++ tests/tools/cuisine/TestCuisineCore.py
@@ -8,6 +8,8 @@ import copy
 from JumpScale import j
 
 
+@patch('JumpScale.core.redis.Redis.hget')
+@patch('JumpScale.core.redis.Redis.hset')
 class TestCuisineCore(unittest.TestCase):
 
     def setUp(self):
@@ -15,21 +17,6 @@ class TestCuisineCore(unittest.TestCase):
             'HOME': '/root',
             'HOSTNAME': 'js8-core',
             'JSBASE': '/js/path',
-            'LD_LIBRARY_PATH': '/opt/jumpscale8//bin',
-            'LESSCLOSE': '/usr/bin/lesspipe %s %s',
-            'LESSOPEN': '| /usr/bin/lesspipe %s',
-            'LS_COLORS': 'rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:',
-            'PATH': '/opt/jumpscale8/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-            'PWD': '/opt/code/github/jumpscale/jumpscale_core8',
-            'PYTHONPATH': '.:/opt/jumpscale8/lib:/opt/jumpscale8/lib/lib-dynload/:/opt/jumpscale8//bin:/opt/jumpscale8//lib/python.zip:/opt/jumpscale8//lib/plat-x86_64-linux-gnu:',
-            'PYTHONUNBUFFERED': '1',
-            'SHLVL': '2',
-            'TERM': 'xterm',
-            '_': '/usr/bin/printenv',
-            '_OLD_LDLIBRARY_PATH': '',
-            '_OLD_PATH': '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-            '_OLD_PS1': '',
-            '_OLD_PYTHONPATH': ''
         }
         self.core = j.tools.cuisine.local.core
         self.dir_paths = {'appDir': '/js/path/apps',
@@ -53,32 +40,29 @@ class TestCuisineCore(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_isJS8Sandbox_property(self):
+    def test_isJS8Sandbox_property(self, cache_set_mock, cache_get_mock):
         """
         Test accessing the isJS8Sandbox property
         """
+        cache_get_mock.return_value = None
         self.assertIsNotNone(self.core.isJS8Sandbox)
 
     @patch('JumpScale.j.tools.cuisine.local.core.getenv')
-    @patch('JumpScale.j.core.db')
-    def test_dir_paths_property_if_JSBASE_and_linux(self, cache_mock, getenv_mock):
+    def test_dir_paths_property_if_JSBASE_and_linux(self, getenv_mock, cache_set_mock, cache_get_mock):
         """
         Happy Path: Test accessing the dir_paths property if JSBASE in env
         """
-        cache_mock.hget.return_value = None
-        cache_mock.set.return_value = None
+        cache_get_mock.return_value = None
         getenv_mock.return_value = self.dump_env
         result = self.core.dir_paths
         self.assertEqual(result, self.dir_paths)
 
     @patch('JumpScale.j.tools.cuisine.local.core.getenv')
-    @patch('JumpScale.j.core.db')
-    def test_dir_paths_property_if_linux(self, cache_mock, getenv_mock):
+    def test_dir_paths_property_if_linux(self, getenv_mock, cache_set_mock, cache_get_mock):
         """
         Happy Path: Test accessing the dir_paths property if JSBASE not found in env
         """
-        cache_mock.hget.return_value = None
-        cache_mock.set.return_value = None
+        cache_get_mock.return_value = None
 
         # remove JSBASE from dump_env
         dump_env = copy.deepcopy(self.dump_env)
@@ -108,13 +92,11 @@ class TestCuisineCore(unittest.TestCase):
 
     @patch('JumpScale.tools.cuisine.CuisineCore.CuisineCore.isMac', new_callable=PropertyMock)
     @patch('JumpScale.j.tools.cuisine.local.core.getenv')
-    @patch('JumpScale.j.core.db')
-    def test_dir_paths_property_if_not_linux(self, cache_mock, getenv_mock, mac_mock):
+    def test_dir_paths_property_if_not_linux(self, getenv_mock, mac_mock, cache_set_mock, cache_get_mock):
         """
         Happy Path: Test accessing the dir_paths property if JSBASE not found in env and not linux
         """
-        cache_mock.hget.return_value = None
-        cache_mock.set.return_value = None
+        cache_get_mock.return_value = None
         mac_mock.return_value = True
 
         # remove JSBASE from dump_env
@@ -146,13 +128,11 @@ class TestCuisineCore(unittest.TestCase):
 
     @patch('JumpScale.tools.cuisine.CuisineCore.CuisineCore.isMac', new_callable=PropertyMock)
     @patch('JumpScale.j.tools.cuisine.local.core.getenv')
-    @patch('JumpScale.j.core.db')
-    def test_dir_paths_property_if_JSBASE_and_not_linux(self, cache_mock, getenv_mock, mac_mock):
+    def test_dir_paths_property_if_JSBASE_and_not_linux(self, getenv_mock, mac_mock, cache_set_mock, cache_get_mock):
         """
         Happy Path: Test accessing the dir_paths property if JSBASE in env and not linux
         """
-        cache_mock.hget.return_value = None
-        cache_mock.set.return_value = None
+        cache_get_mock.return_value = None
         mac_mock.return_value = True
         getenv_mock.return_value = self.dump_env
 

03e7a3b20f48b2e282d91f7652668e496de6c961
Muhamad Azmy <muhamad.azmy@codescalers.com>
2016-09-21 09:44:50 +0200

    Expose number of workers to exetension

--- lib/JumpScale/tools/aggregator/RealityProcess.py
+++ lib/JumpScale/tools/aggregator/RealityProcess.py
@@ -12,13 +12,13 @@ class RealitProcess:
     def __init__(self):
         self.__jslocation__ = "j.tools.realityprocess"
 
-    def influxpump(self, influxdb, cidr='127.0.0.1', ports=[7777], rentention_duration='5d'):
+    def influxpump(self, influxdb, cidr='127.0.0.1', ports=[7777], rentention_duration='5d', workers=4):
         """
         will dump redis stats into influxdb(s)
         get connections from jumpscale clients...
         """
 
-        InfluxDumper(influxdb, cidr=cidr, ports=ports, rentention_duration=rentention_duration).start()
+        InfluxDumper(influxdb, cidr=cidr, ports=ports, rentention_duration=rentention_duration).start(workers=workers)
 
     def monogopump(self, cidr='127.0.0.1', ports=[7777]):
         """

701399cd05aff1ab4c9ed5a45b391b1f347b37d4
Muhamad Azmy <muhamad.azmy@codescalers.com>
2016-10-04 12:20:13 +0200

    Install nodejs

--- lib/JumpScale/tools/cuisine/apps/CuisineNodeJS.py
+++ lib/JumpScale/tools/cuisine/apps/CuisineNodeJS.py
@@ -1,6 +1,4 @@
 from JumpScale import j
-from time import sleep
-
 
 app = j.tools.cuisine._getBaseAppClass()
 
@@ -12,29 +10,10 @@ class CuisineNodeJS(app):
         self._executor = executor
         self._cuisine = cuisine
 
-    def _build(self):
-        # TODO: *3 optional
-        # build nginx
-        raise NotImplementedError
-
-    def install(self, start=True):
+    def install(self):
         """
         can install through ubuntu
-
         """
-        # TODO: *1
-        if start:
-            self.start("?")
 
-    def build(self, start=True, install=True):
-        self._build()
-        if install:
-            self.install(start)
-
-    def start(self, name="???"):
-        # TODO:*1
-        raise NotImplementedError
-
-    def test(self):
-        # host a file test can be reached
-        raise NotImplementedError
+        self._cuisine.package.ensure('nodejs')
+        self._cuisine.package.ensure('npm')

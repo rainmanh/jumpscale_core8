a1bb12102563c0423e57b723b2470941ed08e7e5
Muhamad Azmy <muhamad.azmy@codescalers.com>
2016-10-05 14:57:49 +0200

    Add cuisine rust

---
+++ lib/JumpScale/tools/cuisine/apps/CuisineRust.py
@@ -0,0 +1,28 @@
+from JumpScale import j
+
+app = j.tools.cuisine._getBaseAppClass()
+
+
+class CuisineRust(app):
+    NAME = 'rust'
+
+    def __init__(self, executor, cuisine):
+        self._executor = executor
+        self._cuisine = cuisine
+
+    def install(self):
+        """
+        https://static.rust-lang.org/dist/rust-1.12.0-x86_64-unknown-linux-gnu.tar.gz
+        """
+
+        version = 'rust-1.12.0-x86_64-unknown-linux-gnu'
+        url = 'https://static.rust-lang.org/dist/{}.tar.gz'.format(version)
+        dest = '/tmp/rust.tar.gz'
+        # self._cuisine.core.run('curl -o {} {}'.format(dest, url))
+        self._cuisine.core.run('tar --overwrite -xf {} -C /tmp/'.format(dest))
+
+        # copy file to correct locations.
+        self._cuisine.core.run('mv /tmp/{version} $appDir/rust'.format(version=version))
+
+        self._cuisine.bash.addPath(self._cuisine.core.args_replace('$appDir/rust/rustc/bin'))
+        self._cuisine.bash.addPath(self._cuisine.core.args_replace('$appDir/rust/cargo/bin'))

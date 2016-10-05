3d7e9a3be4c9416b9fd5132beff484af756ac510
Ahmed T. Youssef <xmonader@gmail.com>
2016-10-05 17:52:54 +0200

    WIP for nginx, php, owncloud

--- lib/JumpScale/tools/cuisine/apps/CuisineOwnCloud.py
+++ lib/JumpScale/tools/cuisine/apps/CuisineOwnCloud.py
@@ -7,7 +7,7 @@ class CuisineOwnCloud(app):
 
     NAME = 'owncloud'
 
-    def install(self, start=True, storagepath=""):
+    def install(self, start=True, storagepath="/var/www/html"):
         """
         install owncloud 9.1 on top of nginx/php/tidb
         tidb is the mysql alternative which is ultra reliable & distributed
@@ -16,38 +16,30 @@ class CuisineOwnCloud(app):
         """
 
         C = """
-
-        rm -rf /opt/owncloudbox/httpd/htdocs/owncloud
-
-        cd /tmp && git clone https://github.com/gig-projects/proj_gig_box.git
-
-
-        #TODO: *1 wrong version
-        cd /tmp && wget https://download.owncloud.org/community/owncloud-9.0.0.zip && unzip -a /tmp/owncloud-9.0.0.zip
+        set -xe
+        cd $tmpDir && git clone https://github.com/gig-projects/proj_gig_box.git
+        #cd $tmpDir && wget https://download.owncloud.org/community/owncloud-9.1.1.tar.bz2 && tar jxf owncloud-9.1.1.tar.bz2
+        cd $tmpDir && tar jxf owncloud-9.1.1.tar.bz2
 
         """
-        C = self._cuisine.core.args_replace(C)  # option in execute, no need to do this
         self._cuisine.core.execute_bash(C)
 
         # deploy in $appDir/owncloud
         # use nginx/php other cuisine packages
 
         C = """
-        # copy owncloud under httpd/htdocs
-        /bin/cp -Rf /tmp/owncloud/ /opt/owncloudbox/httpd/htdocs/
-
-        /bin/cp -Rf /tmp/proj_gig_box/ownclouddeployment/owncloud/gig /opt/owncloudbox/httpd/htdocs/ownlcloud/
+        # copy owncloud under {storagepath}
+        mv $tmpDir/owncloud {storagepath}/
 
 
         # copy config.php to new owncloud home httpd/docs
-        /bin/cp -Rf /opt/owncloudbox/proj_gig_box/ownclouddeployment/owncloud/config.php /opt/owncloudbox/httpd/htdocs/owncloud/config/
+        /bin/cp -Rf $tmpDir/proj_gig_box/ownclouddeployment/owncloud/config.php {storagepath}/owncloud/config/
         # copy gig theme
-        /bin/cp -Rf /opt/owncloudbox/proj_gig_box/ownclouddeployment/owncloud/gig /opt/owncloudbox/httpd/htdocs/owncloud/themes/
-        chown -R www-data:www-data /opt/owncloudbox/owncloud/
-        chmod 770 -R /opt/owncloudbox/owncloud
+        /bin/cp -Rf $tmpDir/proj_gig_box/ownclouddeployment/owncloud/gig {storagepath}/owncloud/themes/
+        chown -R www-data:www-data {storagepath}/owncloud/
+        chmod 777 -R /var/www/html/owncloud/config
 
-        """
-        C = self._cuisine.core.args_replace(C)
+        """.format(storagepath=storagepath)
         self._cuisine.core.execute_bash(C)
 
         # look at which owncloud plugins to enable(pdf, ...)
--- lib/JumpScale/tools/cuisine/apps/CuisinePHP.py
+++ lib/JumpScale/tools/cuisine/apps/CuisinePHP.py
@@ -7,40 +7,109 @@ class CuisinePHP(app):
 
     NAME = 'php'
 
-    def build(self):
-        C = """
-        rm -rf /opt/owncloudbox/php
-        cd /opt/owncloudbox && wget http://be2.php.net/distributions/php-7.0.11.tar.bz2 && tar xvjf /opt/owncloudbox/php-7.0.11.tar.bz2
+    def build(self, **config):
+        # **config should include every option as option=value
+        # prefix=$appDir/php
+        # exec_prefix=$appDir/php
+        # https://www.howtoforge.com/tutorial/installing-nginx-with-php7-fpm-and-mysql-on-ubuntu-16.04-lts-lemp/
 
-        mv /opt/owncloudbox/php-7.0.11 /opt/owncloudbox/php
+        ## WIP:
+
+        #TODO: make sure to install multibyte, zip modules
+        # needed modules
+        defaultconfig = {}
+        defaultconfig['enable-zend-multibyte'] = True
+        defaultconfig['with-gd'] = True
+        defaultconfig['with-jpeg'] = True
+        defaultconfig['with-curl'] = True  # apt-get install libcurl4-openssl-dev
+
+        defaultconfig['with-zlib'] = True
+        defaultconfig['with-libzip'] = True
+        defaultconfig['enable-fpm'] = True
+        defaultconfig['enable-opcache'] = True
+        defaultconfig['prefix'] = "$appDir/php"
+        defaultconfig['exec-prefix'] = "$appDir/php"
+
+        config.update(defaultconfig)
+
+        args_string = ""
+        for k, v in config.items():
+            k = k.replace("_", "-")
+            if v is True:
+                args_string += " --{k}".format(k=k)
+            else:
+                args_string += " --{k}={v}".format(k=k, v=v)
+
+        #print("args string: ", args_string)
+        C = """
+        cd $tmpDir && wget http://be2.php.net/distributions/php-7.0.11.tar.bz2 && tar xvjf $tmpDir/php-7.0.11.tar.bz2
+        #cd $tmpDir && tar xvjf $tmpDir/php-7.0.11.tar.bz2
+        mv $tmpDir/php-7.0.11 $tmpDir/php
 
         #build
-        cd /opt/owncloudbox/php && ./configure --with-apxs2=/opt/owncloudbox/httpd/support/apxs --prefix=/opt/owncloudbox/php/ --exec-prefix=/opt/owncloudbox/php/ --with-config-file-scan-dir=/opt/owncloudbox/php/lib && make
+        cd $tmpDir/php && ./configure {args_string} && make
 
-        #echo to httpdconf mimetypes
-        echo "\napplication/x-httpd-php phtml pwml php5 php4 php3 php2 php inc htm html" >> /opt/owncloudbox/httpd/conf/mime.types
+        """.format(args_string=args_string)
 
-        """
+        self._cuisine.core.dir_ensure("$appDir/php")
         C = self._cuisine.core.args_replace(C)
+        #print(C)
         self._cuisine.core.execute_bash(C)
+
 
         # build to $tmpDir/php/
 
         # mysql module
         # postgresql module
         # check modules on:
-        # https://www.howtoforge.com/tutorial/installing-nginx-with-php7-fpm-and-mysql-on-ubuntu-16.04-lts-lemp/
+        #
         # check if we need an php accelerator: https://en.wikipedia.org/wiki/List_of_PHP_accelerators
 
     def install(self, start=True):
+        fpmdefaultconf = """
+include=$appDir/php/etc/php-fpm.d/*.conf
+        """
+        fpmwwwconf = """
+;nobody Start a new pool named 'www'.
+[www]
+
+;prefix = /path/to/pools/$pool
+
+user =  www-data
+group = www-data
+
+listen = 127.0.0.1:9000
+
+listen.allowed_clients = 127.0.0.1
+
+pm = dynamic
+pm.max_children = 5
+pm.start_servers = 2
+pm.min_spare_servers = 1
+pm.max_spare_servers = 3
+        """
+        # make sure to save that configuration file ending with .conf under php/etc/php-fpm.d/www.conf
         C = """
-        cd /opt/owncloudbox/php && make install
+        cd $tmpDir/php && make install
         #NOT MAKE INSTALL
         """
+
+
         C = self._cuisine.core.args_replace(C)
         self._cuisine.core.execute_bash(C)
-
+        fpmdefaultconf = self._cuisine.core.args_replace(fpmdefaultconf)
+        fpmwwwconf = self._cuisine.core.args_replace(fpmwwwconf)
+        self._cuisine.core.file_write("$appDir/php/etc/php-fpm.conf.default", content=fpmdefaultconf)
+        self._cuisine.core.file_write("$appDir/php/etc/php-fpm.d/www.conf", content=fpmwwwconf)
         # copy right files of php: $appDir & libraries
+
+    def start(self):
+        phpfpmbinpath = '$appDir/php/sbin'
+
+        phpfpmcmd = "$appDir/php/sbin/php-fpm -F -y $appDir/php/etc/php-fpm.conf.default"  # foreground
+        phpfpmcmd = self._cuisine.core.args_replace(phpfpmcmd)
+        print("CMD: ", phpfpmcmd)
+        self._cuisine.processmanager.ensure(name="php-fpm", cmd=phpfpmcmd, path=phpfpmbinpath)
 
     def test(self):
         # TODO: *1

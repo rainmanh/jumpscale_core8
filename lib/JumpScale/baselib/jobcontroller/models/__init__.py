a7ca095b0f8f140b7c3ab47858d75aff04f257b9
Khaled Karam <mohamedk@greenitglobe.com>
2016-10-10 17:33:46 +0200

    load redis config from config file jobcontroller's model

--- lib/JumpScale/baselib/jobcontroller/models/__init__.py
+++ lib/JumpScale/baselib/jobcontroller/models/__init__.py
@@ -8,20 +8,39 @@ from .JobModel import JobModel
 from .ActionModel import ActionModel
 from .RunModel import RunModel
 
+defaultConfig = {
+    'redis': {
+        'unixsocket': '/tmp/ays.sock'
+    }
+}
+
 
 class ModelsFactory():
 
     def __init__(self):
+        self._config_path = j.sal.fs.joinPaths(j.dirs.cfgDir, 'ays/ays.conf')
+        config = self._load_config(self._config_path)
+
         self.namespacePrefix = "jobs"
         ModelFactory = j.data.capnp.getModelFactoryClass()
 
         self.capnpModel = ModelCapnp
 
-        self.job = ModelFactory(self, "Job", JobModel)
-        self.action = ModelFactory(self, "Action", ActionModel)
-        self.run = ModelFactory(self, "Run", RunModel)
+        self.job = ModelFactory(self, "Job", JobModel, **config['redis'])
+        self.action = ModelFactory(self, "Action", ActionModel, **config['redis'])
+        self.run = ModelFactory(self, "Run", RunModel, **config['redis'])
 
     def destroy(self):
         self.job.destroy()
         self.action.destroy()
         self.run.destroy()
+
+    def _load_config(self, path):
+        if not j.sal.fs.exists(path):
+            return defaultConfig
+
+        cfg = j.data.serializer.toml.load(path)
+        if 'redis' not in cfg:
+            return defaultConfig
+
+        return cfg

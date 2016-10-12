47f62154c04fd6af220cb21ed2f3c1c61edc7dc6
christophe de Carvalho Pereira Martincs <christophe.dcpm@gmail.com>
2016-10-12 12:56:48 +0400

    Add support for creating AYS client from config file

--- lib/JumpScale/clients/atyourservice/Factory.py
+++ lib/JumpScale/clients/atyourservice/Factory.py
@@ -1,5 +1,5 @@
 from JumpScale.clients.atyourservice.Client import Client
-
+from JumpScale import j
 
 class Factory:
 
@@ -8,3 +8,9 @@ class Factory:
 
     def get(self, host='localhost', port=6379, unixsocket=None):
         return Client(host=host, port=port, unixsocket=unixsocket)
+
+    def getFromConfig(self, config_path):
+        cfg = j.data.serializer.toml.load(config_path)
+        if 'redis' not in cfg:
+            raise j.exceptions.Input('format of the config file not valid. Missing redis section')
+        return Client(host=cfg['redis']['host'], port=cfg['redis']['port'], unixsocket=cfg['redis']['unixsocket'])

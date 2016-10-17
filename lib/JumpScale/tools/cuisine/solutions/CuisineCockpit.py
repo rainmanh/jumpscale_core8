2d4a2770123c324b2bee4c11bc5b9683f390a10f
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2016-10-17 14:27:36 +0400

    quick refactor of cockpit ays api install

--- lib/JumpScale/tools/cuisine/solutions/CuisineCockpit.py
+++ lib/JumpScale/tools/cuisine/solutions/CuisineCockpit.py
@@ -10,32 +10,18 @@ class CuisineCockpit(base):
         self._executor = executor
         self._cuisine = cuisine
 
-    def install(self, start=True, bot_token='', jwt_key='', organization='', client_secret='', client_id='',
-                redirect_uri='', itsyouonlinehost='https://itsyou.online', grafana_scriptedagent=True):
-        """
-        Install cockpit
-        If start is True, bot_token, jwt_key, organization should be specified
-        """
-        self._cuisine.bash.environSet("LC_ALL", "C.UTF-8")
-        # if not self._cuisine.core.isMac and not self._cuisine.core.isCygwin:
-        #     self._cuisine.development.js8.install()
-        #     self._cuisine.development.pip.packageUpgrade("pip")
-
+    def install(self, start=True):
         self.install_deps()
-        self._cuisine.development.git.pullRepo('https://github.com/Jumpscale/jscockpit.git')
-        self.link_code(grafana_scriptedagent=grafana_scriptedagent)
-
+        self._cuisine.development.git.pullRepo('https://github.com/Jumpscale/jscockpit')
+        self._cuisine.core.dir_ensure('/opt/jumpscale8/apps/ays_api/')
+        self._cuisine.core.file_copy('/opt/code/github/jumpscale/jscockpit/jscockpit/api_server', '/opt/jumpscale8/apps/ays_api/api_server')
+        self._cuisine.core.file_copy('/opt/code/github/jumpscale/jscockpit/jscockpit/ays_api/', '/opt/jumpscale8/apps/ays_api/ays_api', recursive=True)
         if start:
-            self.start(bot_token, jwt_key, organization, client_secret, client_id, redirect_uri, itsyouonlinehost)
+            self.start()
 
-    def start(self, bot_token='', jwt_key='', organization='', client_secret='',
-              client_id='', redirect_uri='', itsyouonlinehost='https://itsyou.online'):
-        """
-        bot_token: telegram token for cockpit bot
-        """
-        self.create_config(bot_token, jwt_key, organization, client_secret, client_id, redirect_uri, itsyouonlinehost)
-        cmd = 'jspython cockpit start --config $cfgDir/cockpit/config.toml'
-        self._cuisine.processmanager.ensure('cockpit', cmd=cmd, path='/opt/jumpscale8/apps/cockpit')
+    def start(self, name='cockpit'):
+        cmd = 'jspython api_server'
+        self._cuisine.processmanager.ensure('cockpit', cmd=cmd, path='/opt/jumpscale8/apps/ays_api')
 
     def install_deps(self):
         deps = """
@@ -46,42 +32,3 @@ class CuisineCockpit(base):
         python-telegram-bot
         """
         self._cuisine.development.pip.multiInstall(deps, upgrade=True)
-
-    def link_code(self, grafana_scriptedagent=True):
-        self._cuisine.core.dir_ensure('$appDir')
-        self._cuisine.core.file_link('$codeDir/github/jumpscale/jscockpit/jscockpit/', '$appDir/cockpit')
-        if grafana_scriptedagent:
-            self._cuisine.core.dir_ensure('$tmplsDir/cfg/grafana/public/dashboards/')
-            self._cuisine.core.file_copy('$codeDir/github/jumpscale/jscockpit/apps/Cockpit/.files/scriptedagent.js',
-                                         '$tmplsDir/cfg/grafana/public/dashboards/scriptedagent.js')
-
-    def create_config(self, bot_token, jwt_key, organization, client_secret, client_id,
-                      redirect_uri, itsyouonlinehost='https://itsyou.online'):
-        cfg = {
-            'oauth': {
-                'client_secret': client_secret,
-                'client_id': client_id,
-                'redirect_uri': redirect_uri,
-                'organization': organization,
-                'jwt_key': jwt_key,
-                'itsyouonlinehost': itsyouonlinehost,
-            },
-            'api': {
-                'ays': {
-                    'host': 'localhost',
-                    'port': 5000,
-                    'active': True,
-                }
-            },
-            'bot': {
-                'token': bot_token,
-                'active': True,
-            },
-            'mail': {
-                'port': 25,
-                'active': True,
-            }
-        }
-        self._cuisine.core.createDir('$cfgDir/cockpit')
-        content = j.data.serializer.toml.dumps(cfg)
-        self._cuisine.core.file_write('$cfgDir/cockpit/config.toml', content)

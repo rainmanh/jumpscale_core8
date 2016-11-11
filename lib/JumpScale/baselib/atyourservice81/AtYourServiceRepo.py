a9f5ea1bb84fd7e1db55dd070bb0b62943919d2b
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2016-11-11 13:01:53 +0400

    make sure to overwrite global actor tempaltes with local one from repository

--- lib/JumpScale/baselib/atyourservice81/AtYourServiceRepo.py
+++ lib/JumpScale/baselib/atyourservice81/AtYourServiceRepo.py
@@ -123,7 +123,7 @@ class AtYourServiceRepo():
         # load local templates
         path = j.sal.fs.joinPaths(self.path, "actorTemplates")
         if j.sal.fs.exists(path):
-            for template in j.atyourservice._actorTemplatesGet(self.git, path=path, aysrepo=self):
+            for template in j.atyourservice._actorTemplatesGet(self.git, path=path, aysrepo=self, result=[]):
                 # here we want to overrides the global templates with local one. so having duplicate name is normal
                 templates[template.name] = template
 

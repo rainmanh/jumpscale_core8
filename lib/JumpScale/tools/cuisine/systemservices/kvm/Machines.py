07a08f95e93a6f4df53fdcf15df091b34b96404a
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2016-11-25 16:13:21 +0400

    allow to pass storage pool name to kvm machine creatoin

--- lib/JumpScale/tools/cuisine/systemservices/kvm/Machines.py
+++ lib/JumpScale/tools/cuisine/systemservices/kvm/Machines.py
@@ -7,7 +7,8 @@ class Machines:
     def __init__(self, controller):
         self._controller = controller
 
-    def create(self, name, os='xenial-server-cloudimg-amd64-uefi1.img', disks=[10],
+    def create(self, name, os='xenial-server-cloudimg-amd64-uefi1.img',
+               disks=[10], storage_pool='vms',
                nics=['vms1'], memory=512, cpucount=1,
                cloud_init=True, username="root", passwd="gig1234", sshkey=None, resetPassword=True,
                start=True):
@@ -16,6 +17,7 @@ class Machines:
         @param os str: os name to use.
         @param disks list of int: size of disk names to be used by the machine.
                                   first of the list is the size of the boot disk, remaining are data disks
+        @param storage_pool str: name of the storage to use when creating disks
         @param nics [str]: name of networks to be used with machine. need to be created before. (cuisine.systemservices.kvm.networks.create())
         @param memory int: disk memory in Mb.
         @param cpucount int: number of cpus to use.
@@ -29,7 +31,7 @@ class Machines:
         @param pubkey is the key which will be used to get access to this kvm, if none then use the std ssh key as used for docker
         """
         machine = j.sal.kvm.CloudMachine(self._controller, name, os, disks,
-                                         nics, memory, cpucount, cloud_init=cloud_init)
+                                         nics, memory, cpucount, poolname=storage_pool, cloud_init=cloud_init)
 
         machine.create(username=username, passwd=passwd, sshkey=sshkey)
 

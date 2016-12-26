776daea5ac6f71a3cf5f52217bd6cfbea7102999
Ahmed T. Youssef <xmonader@gmail.com>
2016-12-26 11:58:11 +0200

    Ays httpsownclouds3 129 (#667)
    * function to convert ip address to number
    
    * dockercompose should check for docker-compose file not docker

--- lib/JumpScale/tools/cuisine/systemservices/CuisineDockerCompose.py
+++ lib/JumpScale/tools/cuisine/systemservices/CuisineDockerCompose.py
@@ -6,7 +6,7 @@ app = j.tools.cuisine._getBaseAppClass()
 
 
 class CuisineDockerCompose(app):
-    NAME = "docker"
+    NAME = "docker-compose"
 
     def __init__(self, executor, cuisine):
         self._executor = executor

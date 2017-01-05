e608dbd430c877b19c86dc8c9295611362401d61
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-01-05 13:57:48 +0100

    update cuisine module for tidb

--- lib/JumpScale/tools/cuisine/development/CuisineRust.py
+++ lib/JumpScale/tools/cuisine/development/CuisineRust.py
@@ -6,10 +6,12 @@ app = j.tools.cuisine._getBaseAppClass()
 class CuisineRust(app):
     NAME = 'rust'
 
-    def install(self):
+    def install(self, reset=False):
         """
         https://static.rust-lang.org/dist/rust-1.12.0-x86_64-unknown-linux-gnu.tar.gz
         """
+        if self.doneGet("install") and not reset:
+            return
 
         version = 'rust-nightly-x86_64-unknown-linux-gnu'
         url = 'https://static.rust-lang.org/dist/{}.tar.gz'.format(version)
@@ -21,4 +23,7 @@ class CuisineRust(app):
         self.cuisine.core.run(
             'cd /tmp/{version} && ./install.sh --prefix=$JSAPPSDIR/rust --destdir==$JSAPPSDIR/rust'.format(version=version))
 
-        self.cuisine.bash.addPath(self.replace('$JSAPPSDIR/rust/bin'))
+        self.cuisine.bash.profileDefault.addPath(self.replace('$JSAPPSDIR/rust/bin'))
+        self.cuisine.bash.profileDefault.save()
+
+        self.doneSet('install')

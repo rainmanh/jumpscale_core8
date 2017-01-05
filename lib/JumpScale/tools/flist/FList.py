7261fb4434feb7d8dbb7594044cfa77aeee1d5f8
Maxime Daniel <root@maxux.net>
2017-01-05 10:31:35 +0100

    push flist staging change, this is unstable

--- lib/JumpScale/tools/flist/FList.py
+++ lib/JumpScale/tools/flist/FList.py
@@ -153,6 +153,7 @@ class FList(object):
                 ffiles = []
                 llinks = []
                 sspecials = []
+
                 for fname in files:
                     relPathWithName = os.path.join(dirRelPath, fname)
                     pathAbsolute = os.path.join(self.rootpath, relPathWithName)
@@ -164,15 +165,23 @@ class FList(object):
                         st_mode = stat.st_mode
 
                         if S_ISLNK(st_mode):
-                            destlink = os.readlink(pathAbsolute)
+                            # print("LINK")
+                            # Checking absolute path, relative may fail
+                            destlink = os.path.realpath(pathAbsolute)
                             if not destlink.startswith(self.rootpath):  # check if is link & if outside of FS
+                                print("OUTSIDE")
                                 ffiles.append((fname, stat))  # are links which point to outside of fs
                             else:
+                                print("INSIDE")
+                                # Ensure relative path works
+                                # destlink = os.readlink(pathAbsolute)
                                 llinks.append((fname, stat, destlink))
                         else:
                             if S_ISREG(st_mode):
+                                # print("REGULAR")
                                 ffiles.append((fname, stat))
                             else:
+                                # print("SPECIAL")
                                 sspecials.append((fname, stat))
 
                 # initialize right amount of objects in capnp
@@ -192,15 +201,16 @@ class FList(object):
                 for fname, stat, destlink in llinks:
                     obj = ddir.dbobj.files[counter]
                     relPath, dirkey = self.path2key(destlink)
-                    obj.destDirKey = dirkey  # link to other directory
-                    obj.destname = j.sal.fs.getBaseName(destlink)
+                    # FIXME
+                    # obj.destDirKey = dirkey  # link to other directory
+                    # obj.destname = j.sal.fs.getBaseName(destlink)
                     self._setMetadata(obj, stat, fname)
                     counter += 1
 
                 # process special files
                 counter = 0
-                for fullpathSub, stat, destlink in llinks:
-                    obj = ddir.dbobj.files[counter]
+                for fname, stat in sspecials:
+                    obj = ddir.dbobj.specials[counter]
                     # testing special files type
                     if S_ISSOCK(stat.st_mode):
                         obj.type = "socket"
@@ -248,8 +258,9 @@ class FList(object):
         dbobj.name = fname
         # j.sal.fs.getBaseName(fpath)
 
-        dbobj.modificationTime = stat.st_mtime
-        dbobj.creationTime = stat.st_ctime
+        dbobj.modificationTime = int(stat.st_mtime)
+        dbobj.creationTime = int(stat.st_ctime)
+        dbobj.size = stat.st_size
 
         uname = pwd.getpwuid(stat.st_uid).pw_name
         # uname_id = stat.st_uid
@@ -261,18 +272,10 @@ class FList(object):
         aci.dbobj.gname = gname
         aci.dbobj.mode = stat.st_mode
 
-        if self.aciCollection.exists(aci.key):
-            aci2 = self.aciCollection.get(aci.key)
-            id = aci2.id
-        else:
-            id = int(aci.key[0: 4], 16)
-            while self.aciCollection.lookup(id) != None:  # means exists
-                id += 1
-
-            aci.dbobj.id = id
+        if not self.aciCollection.exists(aci.key):
             aci.save()
 
-        dbobj.aclkey = id
+        dbobj.aclkey = aci.key
 
     def walk(self, dirFunction=None, fileFunction=None, specialFunction=None, linkFunction=None, args={}, currentDirKey="", dirRegex=[], fileRegex=[], types="DFLS"):
         """
@@ -391,14 +394,17 @@ class FList(object):
             return
 
         for item in ddir.dbobj.files:
+            print(item)
             if valid(j.sal.fs.joinPaths(ddir.dbobj.location, item.name), fileRegex) and "F" in types:
                 fileFunction(dirobj=ddir, type="F", name=item.name, subobj=item, args=args)
 
         for item in ddir.dbobj.links:
-            if validd(j.sal.fs.joinPaths(ddir.dbobj.location, item.name), fileRegex) and "L" in types:
+            print(item)
+            if valid(j.sal.fs.joinPaths(ddir.dbobj.location, item.name), fileRegex) and "L" in types:
                 linkFunction(dirobj=ddir, type="L", name=item.name, subobj=item, args=args)
 
         for item in ddir.dbobj.specials:
+            print(item)
             if valid(j.sal.fs.joinPaths(ddir.dbobj.location, item.name), fileRegex and "S" in types):
                 specialFunction(dirobj=ddir, type="S", name=item.name, subobj=item, args=args)
 
@@ -427,8 +433,16 @@ class FList(object):
         result["nrlinks"] = 0
         result["nrdirs"] = 0
         result["nrspecial"] = 0
-        self.walk(dirFunction=procDir, fileFunction=procFile,
-                  specialFunction=procSpecial, linkFunction=procLink, args=result, dirRegex=dirRegex, fileRegex=fileRegex, types=types)
+        self.walk(
+            dirFunction=procDir,
+            fileFunction=procFile,
+            specialFunction=procSpecial,
+            linkFunction=procLink,
+            args=result,
+            dirRegex=dirRegex,
+            fileRegex=fileRegex,
+            types=types
+        )
 
         return (result["size"], result["nrfiles"], result["nrdirs"], result["nrlinks"], result["nrspecial"])
 
@@ -447,38 +461,88 @@ class FList(object):
             print("%s/%s (%s)" % (dirobj.dbobj.location, name, type))
 
         result = []
-        self.walk(dirFunction=procDir, fileFunction=procFile,
-                  specialFunction=procSpecial, linkFunction=procLink, args=result, dirRegex=dirRegex, fileRegex=fileRegex, types=types)
+        self.walk(
+            dirFunction=procDir,
+            fileFunction=procFile,
+            specialFunction=procSpecial,
+            linkFunction=procLink,
+            args=result,
+            dirRegex=dirRegex,
+            fileRegex=fileRegex,
+            types=types
+        )
 
     def dumps(self, dirRegex=[], fileRegex=[], types="DFLS"):
         """
         dump to text based flist format
         """
 
-        # TODO: *1
+        # Set common values for all types
+        # Others fields (type, hash, extended) need to be filled by caller
+        def setDefault(dirobj, name, subobj):
+            x = self.aciCollection.get(subobj.aclkey)
+            item = [
+                "%s/%s" % (dirobj.dbobj.location, name), # Path
+                "", # To be filled lated                 # Hash
+                "%d" % subobj.size,                      # Size
+                x.dbobj.uname,                           # User (permissions)
+                x.dbobj.gname,                           # Group (permissions)
+                x.modeInOctFormat,                       # Permission mode
+                "", # To be filled later                 # File type
+                "%d" % subobj.creationTime,              # Creation Timestamp
+                "%d" % subobj.modificationTime,          # Modification Timestamp
+                ""  # To be filled later                 # Extended attributes
+            ]
+
+            return item
 
         def procDir(dirobj, type, name, args, key):
-            print("%s/%s (%s)" % (dirobj.dbobj.location, name, type))
+            item = setDefault(dirobj, name, dirobj.dbobj)
+
+            # Set types (directory)
+            item[6] = "4"
+
+            print("|".join(item))
 
         def procFile(dirobj, type, name, subobj, args):
-            print("%s/%s (%s)" % (dirobj.dbobj.location, name, type))
+            item = setDefault(dirobj, name, subobj)
+
+            # Set filetype
+            item[1] = "** HASH **"
+            item[6] = "2"
+
+            print("|".join(item))
 
         def procLink(dirobj, type, name, subobj, args):
+            print("============== LINK")
+            print(subobj)
+
             print("%s/%s (%s)" % (dirobj.dbobj.location, name, type))
+            print("==============")
 
         def procSpecial(dirobj, type, name, subobj, args):
+            print("============== SPECIAL")
+            print(subobj)
+
             print("%s/%s (%s)" % (dirobj.dbobj.location, name, type))
+            print("==============")
 
         result = []
-        self.walk(dirFunction=procDir, fileFunction=procFile,
-                  specialFunction=procSpecial, linkFunction=procLink, args=result)
+        self.walk(
+            dirFunction=procDir,
+            fileFunction=procFile,
+            specialFunction=procSpecial,
+            linkFunction=procLink,
+            args=result
+        )
 
+        """
         from IPython import embed
         print("DEBUG NOW in dumps")
         embed()
         raise RuntimeError("stop debug here")
 
-        for f in self._data:
+        for f in rawdata:
             p = f[0]
             if p.startswith(trim):
                 p = p[len(trim):]
@@ -489,6 +553,7 @@ class FList(object):
             data.append(line)
 
         return "\n".join(data) + "\n"
+        """
 
     def destroy(self):
         self.aciCollection.destroy()
---
+++ lib/JumpScale/tools/flist/model.capnp
@@ -0,0 +1,102 @@
+@0xae9223e76351538a;
+
+struct Dir {
+
+  #name of dir
+  name @0 :Text;
+
+  #location in filesystem = namespace
+  location @1 :Text;
+
+  files @2 :List(File);
+  struct File{
+      name @0 : Text;
+      #blocksize in bytes = blocksize * 4 KB, blocksize is same for all parts of file
+      #max blocksize = 128 MB
+      blocksize @1 : UInt8;
+      blocks @2 :List(Data); #list of the hashes of the blocks
+      size @3 : UInt64; #in bytes
+      aclkey @4: Text; #is pointer to ACL
+      modificationTime @5: UInt32;
+      creationTime @6: UInt32;
+  }
+
+  dirs @3 :List(SubDir);
+  struct SubDir{
+      name @0 : Text;
+      key @1: Text;
+  }
+
+  links @4 :List(Link); #only for non dirs
+  struct Link{
+      name @0 : Text;
+      # aclkey @1: Text; #is pointer to ACL
+      # destDirKey @2: Text; #key of dir in which destination is
+      # destName @3: Text;
+      target @1: Text;
+      modificationTime @2: UInt32;
+      creationTime @3: UInt32;
+  }
+
+  specials @5 :List(Special);
+  struct Special{
+      name @0 : Text;
+      type @1 :State;
+      # - 0: socket       (S_IFSOCK)
+      # - 1: block device (S_IFBLK)
+      # - 2: char. device (S_IFCHR)
+      # - 3: fifo pipe    (S_IFIFO)
+      enum State {
+        socket @0;
+        block @1;
+        chardev @2;
+        fifopipe @3;
+        unknown @4;
+      }
+      #data relevant for type of item
+      data @2 :Data;
+      modificationTime @3: UInt32;
+      creationTime @4: UInt32;
+      size @5: UInt32;
+      aclkey @6: Text; #is pointer to ACL
+  }
+
+  parent @6 :Text; #dir key of parent
+
+  #metadata for dir
+  size @7 : UInt64; #in bytes
+  aclkey @8: Text; #is pointer to ACL
+  isLink @9: Bool; #if is link and not physically on disk
+  modificationTime @10: UInt32;
+  creationTime @11: UInt32;
+
+
+}
+
+
+struct UserGroup{
+    name @0 : Text;
+    #itsyouonline id
+    iyoId @1 : Text;
+    #itsyouonline unique id per user or group, is globally unique
+    iyoInt @2 : UInt64;
+}
+
+
+struct ACI {
+    #for backwards compatibility with posix
+    uname @0 :Text;
+    gname @1 :Text;
+    mode @2 : UInt16;
+
+    rights @3 :List(Right);
+    struct Right {
+      #text e.g. rwdl- (admin read write delete list -), freely to be chosen
+      #admin means all rights (e.g. on / = namespace or filesystem level all rights for everything)
+      #- means remove all previous ones (is to stop recursion), if usergroupid=0 then is for all users & all groups
+      right @0 :Text;
+      usergroupid @1 : UInt16;
+    }
+
+    id @4 :UInt32;
+}

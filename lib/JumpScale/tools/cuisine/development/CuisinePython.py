2902333f87b596cdb1e3f61634ddec92ab29ffc4
kristof de spiegeleer <kristof@incubaid.com>
2017-01-09 12:35:30 +0400

    installer fixes & cuisine improvements: PYTHON

--- lib/JumpScale/tools/cuisine/development/CuisinePython.py
+++ lib/JumpScale/tools/cuisine/development/CuisinePython.py
@@ -6,19 +6,27 @@ base = j.tools.cuisine._getBaseClass()
 class CuisinePython(base):
 
     def _init(self):
-        self.BUILDDIR = self.replace("$BUILDDIR/python3/")
-        self.CODEDIR = self.replace("$CODEDIR/mercurial/cpython/")
+        self.BUILDDIRL = self.core.replace("$BUILDDIR/python3/")
+        self.CODEDIRL = self.core.replace("$CODEDIR/mercurial/cpython/")
 
-    def build(self,  reset=False): #TODO: *1 building on ovh4, gave issues, test, needs to integrate well with openssl
+    def reset(self):
+        base.reset(self)
+        self.core.dir_remove(self.BUILDDIRL)
+        self.core.dir_remove(self.CODEDIRL)
+        self.cuisine.development.pip.reset()
+
+    def build(self,  reset=False):
         """
         """
-        if self.doneGet("build") and not reset:
-            return
-
-        self.cuisine.development.openssl.build(reset=reset)
 
         if reset:
-            self.cuisine.core.run("rm -rf %s" % self.BUILDDIR)
+            self.reset()
+
+        if self.doneGet("build") and not reset:
+            self.pipAll()
+            return
+
+        self.cuisine.development.openssl.build()
 
         if self.cuisine.core.isMac:
             if not self.doneGet("xcode_install"):
@@ -32,59 +40,114 @@ class CuisinePython(base):
 
         if not self.doneGet("compile") or reset:
             self.cuisine.development.mercurial.pullRepo("https://hg.python.org/cpython", reset=reset)
-            self.cuisine.core.run("set -ex;cd %s;hg update 3.6" % self.CODEDIR)
+            self.cuisine.core.run("set -ex;cd %s;hg update 3.6" % self.CODEDIRL)
 
-            if self.cuisine.core.isMac:
-                openssldir = self.cuisine.development.openssl.BUILDDIR
-                openSSlIncludeLine = ":%s:%s/include" % (openssldir, openssldir)
-                C = 'cd %s;CPPFLAGS="-I%s/include";LDFLAGS="-L%s";./configure' % (self.CODEDIR, openssldir, openssldir)
-            else:
-                C = "cd %s;./configure" % self.CODEDIR
+            if self.core.isMac: #TODO: *2 cant we do something similar for linux?
 
-            self.log("configure python3")
-            self.log(C)
-            self.cuisine.core.file_write("%s/myconfigure.sh" % self.CODEDIR, C, replaceArgs=True)
+                C="""
+                set -ex
+                cd $CODEDIRL
 
-            C = "cd %s;sh myconfigure.sh" % self.CODEDIR
-            self.cuisine.core.run(C)
+                export LIBRARY_PATH="$openssldir/lib"
+                export CPPPATH="$openssldir/include"
+                export CPATH="$openssldir/include"
+                export PATH=$openssldir/lib:/$openssldir/bin:/usr/local/bin:/usr/bin:/bin
 
-            C = "cd %s;make -s -j4" % self.CODEDIR
+                export CFLAGS="-I$CPATH/"
+                export CPPFLAGS="-I$CPATH/"
+                export LDFLAGS="-L$LIBRARY_PATH/"
+
+                echo $CFLAGS
+                echo $LDFLAGS
+
+                ./configure
+                make -s -j4
+
+
+                """
+
+                C=C.replace("$openssldir",self.cuisine.development.openssl.BUILDDIRL)
+                C=self.replace(C)
+
+                self.cuisine.core.file_write("%s/mycompile_all.sh" % self.CODEDIRL, C)
+            else: #TODO: *1 not working compile, see if we can do in line with osx, something wrong with openssl
+                # configure custom location for openssl
+                setup_path = '{}/Modules/Setup'.format(self.CODEDIRL)
+                self.cuisine.core.file_copy(setup_path+".dist", setup_path)
+                content = """
+                    _socket socketmodule.c
+                    SSL=$BUILDDIRL
+                    _ssl _ssl.c -DUSE_SSL -I$(SSL)/include -I$(SSL)/include/openssl -L$(SSL)/lib -lssl -lcrypto
+                    """
+
+                self.cuisine.core.file_write(location=setup_path, content=self.replace(content), append=True)
+
+                C = "cd {codedir};./configure --prefix={builddir}".format(builddir=self.BUILDDIRL, codedir=self.CODEDIRL)
+                self.log("configure python3")
+                self.log(C)
+                self.cuisine.core.file_write("%s/myconfigure.sh" % self.CODEDIRL, C, replaceArgs=True)
+                C = "cd %s;sh myconfigure.sh" % self.CODEDIRL
+                self.cuisine.core.run(C)
+
+                C = "cd %s;make -s -j4; make install" % self.CODEDIRL
+                self.log("compile python3")
+                self.log(C)
+                self.cuisine.core.run(C)
+
+
             self.log("compile python3")
             self.log(C)
             self.cuisine.core.run(C)
+
         self.doneSet("compile")
 
         # find buildpath for lib (depending source it can be other destination)
         # is the core python binaries
         libBuildName = [item for item in self.cuisine.core.run(
-            "ls %s/build" % self.CODEDIR)[1].split("\n") if item.startswith("lib")][0]
-        lpath = j.sal.fs.joinPaths(self.CODEDIR, "build", libBuildName)
-        self.cuisine.core.copyTree(source=lpath, dest=self.BUILDDIR, keepsymlinks=False, deletefirst=False,
+            "ls %s/build" % self.CODEDIRL)[1].split("\n") if item.startswith("lib")][0]
+        lpath = j.sal.fs.joinPaths(self.CODEDIRL, "build", libBuildName)
+        self.cuisine.core.copyTree(source=lpath, dest=self.BUILDDIRL, keepsymlinks=True, deletefirst=False,
                                    overwriteFiles=True, recursive=True, rsyncdelete=False, createdir=True)
 
         # copy python libs (non compiled)
-        ignoredir = ["test", "tkinter", "turtledemo",
+        ignoredir = [ "tkinter", "turtledemo",
                      "msilib", "pydoc*", "lib2to3", "idlelib"]
-        lpath = self.replace("$CODEDIR/lib")
-        ldest = self.replace("$BUILDDIR/plib")
-        self.cuisine.core.copyTree(source=lpath, dest=ldest, keepsymlinks=False, deletefirst=False,
+        lpath = self.replace("$CODEDIRL/Lib")
+        ldest = self.replace("$BUILDDIRL/plib")
+        self.cuisine.core.copyTree(source=lpath, dest=ldest, keepsymlinks=True, deletefirst=False,
                                    overwriteFiles=True, ignoredir=ignoredir,
                                    recursive=True, rsyncdelete=True, createdir=True)
 
-        self.cuisine.core.file_copy("%s/python.exe" % self.CODEDIR, "%s/python3" % self.BUILDDIR)
+        if self.core.isMac:
+            self.cuisine.core.file_copy("%s/python.exe" % self.CODEDIRL, "%s/python3" % self.BUILDDIRL)
+        else:
+            self.cuisine.core.file_copy("%s/python" % self.CODEDIRL, "%s/python3" % self.BUILDDIRL)
+
+
+        C="""
+            cd $BUILDDIRL
+            mkdir bin
+            mv python3 bin/python3
+            ln -s bin/python3 python3
+        """
+        self.cuisine.core.run(self.replace(C))
+
 
         # copy includes
-        lpath = j.sal.fs.joinPaths(self.CODEDIR, "Include",)
-        ldest = j.sal.fs.joinPaths(self.BUILDDIR, "include/python")
-        self.cuisine.core.copyTree(source=lpath, dest=ldest, keepsymlinks=False, deletefirst=False,
+        lpath = j.sal.fs.joinPaths(self.CODEDIRL, "Include",)
+        ldest = j.sal.fs.joinPaths(self.BUILDDIRL, "include/python")
+        self.cuisine.core.copyTree(source=lpath, dest=ldest, keepsymlinks=True, deletefirst=False,
                                    overwriteFiles=True, ignoredir=ignoredir,
                                    recursive=True, rsyncdelete=False, createdir=True)
 
         # now copy openssl parts in
-        self.cuisine.core.copyTree(source=self.cuisine.development.openssl.BUILDDIR, dest=self.BUILDDIR,
-                                   keepsymlinks=False, deletefirst=False,
+        self.cuisine.core.copyTree(source=self.cuisine.development.openssl.BUILDDIRL, dest=self.BUILDDIRL,
+                                   keepsymlinks=True, deletefirst=False,
                                    overwriteFiles=True, ignoredir=ignoredir,
                                    recursive=True, rsyncdelete=False, createdir=True)
+
+        self.cuisine.core.file_copy("%s/pyconfig.h" % self.CODEDIRL, "%s/include/python/pyconfig.h" % self.BUILDDIRL)
+
 
         C = """
 
@@ -108,12 +171,12 @@ class CuisinePython(base):
         fi
         """
 
-        self.cuisine.core.file_write("%s/env.sh" % self.BUILDDIR, C, replaceArgs=True)
+        self.cuisine.core.file_write("%s/env.sh" % self.BUILDDIRL, C, replaceArgs=True)
 
         if not self.doneGet("pip3install") or reset:
             C = """
             set -ex
-            cd $BUILDDIR
+            cd $BUILDDIRL
             source env.sh
             rm -rf get-pip.py
             curl https://bootstrap.pypa.io/get-pip.py > get-pip.py
@@ -122,7 +185,67 @@ class CuisinePython(base):
             self.cuisine.core.run(self.replace(C))
         self.doneSet("pip3install")
 
-        # needs at least /JS8/code/github/jumpscale/jumpscale_core8/install/dependencies.py
+        self.pipAll()
+
+        msg = "\n\nto test do:\ncd $BUILDDIRL;source env.sh;python3"
+        msg = self.replace(msg)
+        self.log(msg)
+        self.doneSet("build")
+
+    def sandbox(self, reset=False,deps=True):
+        if deps:
+            self.build(reset=reset)
+        if self.doneGet("sandbox") and not reset:
+            return
+
+        C = """
+        set -ex
+        cd $BUILDDIRL
+
+        rm -rf share
+        mkdir -p lib/python3.6/site-packages/
+        rsync -rav lib/python3.6/site-packages/ plib/site-packages/
+        rm -rf lib/python3.6
+        find . -name '*.pyc' -delete
+
+        find . -name 'get-pip.py' -delete
+        set +ex
+        find -L .  -name '__pycache__' -exec rm -rf {} \;
+        find . -name "*.dist-info" -exec rm -rf {} \;
+        find . -name "*.so" -exec mv {} lib/ \;
+
+        # rm -f _sysconfigdata_m_darwin_darwin.py
+        rm -f openssl*
+
+        """
+        self.cuisine.core.run(self.replace(C))
+
+        # now copy jumpscale in
+        linkpath = "%s/lib/JumpScale" % self.cuisine.core.dir_paths["JSBASEDIR"]
+        C = "ln -s %s %s/lib/JumpScale" % (linkpath, self.BUILDDIRL)
+        if not self.cuisine.core.file_exists("%s/lib/JumpScale" % self.BUILDDIRL):
+            self.cuisine.core.run(C)
+
+        # # now create packaged dir
+        # destpath2 = self.BUILDDIRL.rstrip("/").rstrip() + "2"
+        # self.cuisine.core.copyTree(source=self.BUILDDIRL, dest=destpath2, keepsymlinks=False, deletefirst=True,
+        #                            overwriteFiles=True,
+        #                            recursive=True, rsyncdelete=True, createdir=True)
+
+        # zip trick does not work yet lets leave for now
+        # C = """
+        # set -ex
+        # cd %s/plib
+        # zip -r ../plib.zip *
+        # cd ..
+        # rm -rf plib
+        # """ % destpath2
+        # self.cuisine.core.run(C)
+
+        # self.doneSet("sandbox")
+
+    def pipAll(self,reset=False):
+        # needs at least items from /JS8/code/github/jumpscale/jumpscale_core8/install/dependencies.py
         C = """
         uvloop
         redis
@@ -131,7 +254,6 @@ class CuisinePython(base):
         gitpython
         click
         pymux
-        uvloop
         pyyaml
         ipdb
         requests
@@ -150,64 +272,10 @@ class CuisinePython(base):
         ipfsapi
         curio
         uvloop
+        gevent
         """
         self.pip(C, reset=reset)
-
-        msg = "to test do:\ncd $BUILDDIR;source env.sh;python3"
-        msg = self.replace(msg)
-        self.log(msg)
-        self.doneSet("build")
-
-    def sandbox(self, reset=False):
-        self.build(reset=reset)
-        if self.doneGet("sandbox") and not reset:
-            return
-
-        from IPython import embed
-        print("DEBUG NOW sandbox")
-        embed()
-        raise RuntimeError("stop debug here")
-
-        C = """
-        set -ex
-        cd $BUILDDIR
-        rm -rf share
-        rsync -rav lib/python3.6/site-packages/ plib/site-packages/
-        rm -rf lib/python3.6
-        find . -name '*.pyc' -delete
-
-        find . -name 'get-pip.py' -delete
-        set +ex
-        find -L .  -name '__pycache__' -exec rm -rf {} \;
-        find . -name "*.dist-info" -exec rm -rf {} \;
-        find . -name "*.so" -exec mv {} lib/ \;
-
-        """
-        self.cuisine.core.run(self.replace(C))
-
-        # now copy jumpscale in
-        linkpath = "%s/lib/JumpScale" % self.cuisine.core.dir_paths["base"]
-        C = "ln -s %s %s/lib/JumpScale" % (linkpath, self.BUILDDIR)
-        if not self.cuisine.core.file_exists("%s/lib/JumpScale" % self.BUILDDIR):
-            self.cuisine.core.run(C)
-
-        # now create packaged dir
-        destpath2 = self.BUILDDIR.rstrip("/").rstrip() + "2"
-        self.cuisine.core.copyTree(source=self.BUILDDIR, dest=destpath2, keepsymlinks=False, deletefirst=True,
-                                   overwriteFiles=True,
-                                   recursive=True, rsyncdelete=True, createdir=True)
-
-        # zip trick does not work yet lets leave for now
-        # C = """
-        # set -ex
-        # cd %s/plib
-        # zip -r ../plib.zip *
-        # cd ..
-        # rm -rf plib
-        # """ % destpath2
-        # self.cuisine.core.run(C)
-
-        self.doneSet("sandbox")
+        self.sandbox(deps=False)
 
     def pip(self, pips, reset=False):
         for item in pips.split("\n"):
@@ -215,8 +283,8 @@ class CuisinePython(base):
             if item == "":
                 continue
             # cannot use cuisine functionality because would not be sandboxed
-            if self.doneGet("pip3_%s" % item) or reset:
-                C = "set -ex;cd $BUILDDIR;source env.sh;pip3 install --trusted-host pypi.python.org %s" % item
+            if not self.doneGet("pip3_%s" % item) or reset:
+                C = "set -ex;cd $BUILDDIRL;source env.sh;pip3 install --trusted-host pypi.python.org %s" % item
                 self.cuisine.core.run(self.replace(C))
                 self.doneSet("pip3_%s" % item)
 

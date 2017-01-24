095c5ddb02124db28daa3f0bb68ec0aa17fc55ce
alichaddad <chaddada@greenitglobe.com>
2017-01-24 14:13:39 +0200

    Added install and start actions

--- lib/JumpScale/tools/cuisine/apps/CuisinePostgresql.py
+++ lib/JumpScale/tools/cuisine/apps/CuisinePostgresql.py
@@ -16,16 +16,45 @@ class CuisinePostgresql(app):
         self._cuisine.core.file_download(postgre_url, dest)
         self._cuisine.core.run('cd $tmpDir; tar xvf $tmpDir/postgresql-9.6.1.tar.gz')
         cmd = """
-        cd $tmpDir
-        cd postgresql-9.6.1
+        cd $tmpDir/postgresql-9.6.1
         ./configure
         make
 
         """
         self._cuisine.core.execute_bash(cmd, profile=True)
 
-    def install(self, reset=False, start=False):
+    def _group_exists(self,groupname):
+        return groupname in open("/etc/group").read()
+
+    def install(self, reset=False, start=False, port=5432):
         if reset is False and self.isInstalled():
             return
+        cmd = """
+        cd $tmpDir/postgresql-9.6.1
+        make install prefix=$appDir/pgsql bindir=$binDir/postgres sysconfdir=$cfgDir libdir=$libDir/postgres with-pgport=%s
+        """ % str(port)
+
+        self._cuisine.core.dir_ensure("$appDir/pgsql")
+        self._cuisine.core.dir_ensure("$binDir/postgres")
+        self._cuisine.core.dir_ensure("$libDir/postgres")
+        self._cuisine.core.execute_bash(cmd, profile=True)
+        if not self._group_exists("postgres"):
+            self._cuisine.core.run('adduser --system --quiet --home $libDir/postgres --no-create-home \
+        --shell /bin/bash --group --gecos "PostgreSQL administrator" postgres')
+        c = """
+        cd $appDir/pgsql
+        mkdir data
+        mkdir log
+        chown postgres $binDir/postgres
+        chown postgres $appDir/pgsql/
+        sudo -u posrgres $binDir/postgres/initdb -D $appDir/pgsql/data
+        """
+        self._cuisine.core.execute_bash(c, profile=True)
         if start:
             self.start()
+
+    def start(self):
+        cmd = """
+        sudo -u postgres $binDir/postgres/pg_ctl -D $appDir/pgsql/data -l $appDir/pgsql/log/logfile start
+        """
+        self._cuisine.core.execute_bash(cmd, profile=True)

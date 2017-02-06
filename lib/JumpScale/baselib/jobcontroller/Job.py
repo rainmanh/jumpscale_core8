5c95444448694a7bac2fa488733dd0b019562d0f
christophe de Carvalho Pereira Martins <christophe@greenitglobe.com>
2017-02-06 17:08:18 +0100

    set the state of the job action when starting job

--- lib/JumpScale/baselib/jobcontroller/Job.py
+++ lib/JumpScale/baselib/jobcontroller/Job.py
@@ -271,7 +271,6 @@ class Job():
         """
         # for now use default ThreadPoolExecutor
         loop = asyncio.get_event_loop()
-
         if self.model.dbobj.debug is False:
             self.model.dbobj.debug = self.sourceToExecute.find('ipdb') != -1 or self.sourceToExecute.find('IPython') != -1
 
@@ -284,6 +283,10 @@ class Job():
         # register callback to deal with logs and state of the job after execution
         now = j.data.time.epoch
         self._future.add_done_callback(functools.partial(_execute_cb, self, now))
+
+        if self.service is not None and self.model.dbobj.actionName in self.service.model.actions:
+            service_action_obj = self.service.model.actions[self.model.dbobj.actionName]
+            service_action_obj.state = 'running'
 
         self.model.dbobj.state = 'running'
         self.save()

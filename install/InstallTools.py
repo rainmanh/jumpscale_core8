159e53f1c5f4a7e2737a07f1628a206831d1e806
Reem Khamis <khamisr@codescalers.com>
2017-02-13 17:01:26 +0200

    Fix previous reverted condition

--- install/InstallTools.py
+++ install/InstallTools.py
@@ -2647,7 +2647,7 @@ class InstallTools(GitMethods, FSMethods, ExecutorMethods, SSHMethods, UI):
             if exists("%s/env.sh" % curdir) and exists("%s/js.sh" % (curdir)):
                 env["BASEDIR"] = os.getcwd()
             else:
-                if self.TYPE.startswith("LINUX") and not self.TYPE.startswith("OSX"):  # ON OSX WE ALSO NEED TO SUPPORT /opt !!!
+                if not self.TYPE.startswith("LINUX") and not self.TYPE.startswith("OSX"):  # ON OSX WE ALSO NEED TO SUPPORT /opt !!!
                     env["BASEDIR"] = "%s/opt" % env['HOME']
                 else:
                     env["BASEDIR"] = "/opt"
@@ -2656,7 +2656,7 @@ class InstallTools(GitMethods, FSMethods, ExecutorMethods, SSHMethods, UI):
             env["JSBASE"] = "%s/jumpscale8" % env["BASEDIR"]
 
         if not "VARDIR" in env:
-            if self.TYPE.startswith("LINUX") and not self.TYPE.startswith("OSX"):  # ON OSX WE ALSO NEED TO SUPPORT /opt !!!
+            if not self.TYPE.startswith("LINUX") and not self.TYPE.startswith("OSX"):  # ON OSX WE ALSO NEED TO SUPPORT /opt !!!
                 env["VARDIR"] = "%s/optvar" % env['HOME']
             else:
                 env["VARDIR"] = "/optvar"

760384a10d6252220ccafb540bc707ab85576423
Reem Khamis <khamisr@codescalers.com>
2017-02-16 11:27:56 +0200

    Remove hardcoded hostname in AYS server

--- lib/JumpScale/clients/atyourservice/sync_client/__init__.py
+++ lib/JumpScale/clients/atyourservice/sync_client/__init__.py
@@ -5,7 +5,7 @@ from .client import Client as APIClient
 from .oauth2_client_itsyouonline import Oauth2ClientItsyouonline
 
 class Client:
-    def __init__(self, base_uri="http://js8:5000"):
+    def __init__(self, base_uri="http://localhost:5000"):
         self.api = APIClient(base_uri)
-        
+
         self.oauth2_client_itsyouonline = Oauth2ClientItsyouonline()

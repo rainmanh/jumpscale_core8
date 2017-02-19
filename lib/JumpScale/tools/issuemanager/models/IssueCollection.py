77e3d26f4e4e4a267a119c49070867f719607f27
Reem Khamis <khamisr@codescalers.com>
2017-02-19 17:32:18 +0200

    Fix user filtering in issuemanager

--- lib/JumpScale/tools/issuemanager/models/IssueCollection.py
+++ lib/JumpScale/tools/issuemanager/models/IssueCollection.py
@@ -49,9 +49,8 @@ class IssueCollection(base):
         if assignee == "":
             assignee = ".*"
         else:
-            import ipdb; ipdb.set_trace()
             users = j.tools.issuemanager.getUserCollectionFromDB()
-            assignee_id = users.find(name=assignee)[0].dictFiltered.get('id')
+            assignee_id = users.find(name='despiegk')[0].gogsRefs[0].id
             assignee = ".*%s.*" % assignee_id
 
         if comment == "":

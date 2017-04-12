76f5dcde94229f1a7600aa27dec9121e26b78a9b
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2017-04-12 13:16:34 +0200

    Adds fixes installing pycapnp
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- install/dependencies.py
+++ install/dependencies.py
@@ -65,7 +65,8 @@ class dependencies():
         '''
         if self.do.isAlpine():
             self.do.executeBashScript(C, executor=executor)
-        self.do.pip(["cython", "pycapnp"], executor=executor)
+        self.do.pip("cython", executor=executor)
+        self.do.pip("pycapnp"], executor=executor)
 
     def portal(self, executor=None):
         C = """

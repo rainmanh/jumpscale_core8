f3f337209f55946a67685c13bfa49b9815d4f534
Abdelrahman Hussein <husseina@greenitglobe.com>
2017-04-18 10:54:03 +0000

    remove ipdb statement

--- lib/JumpScale/tools/cuisine/CuisineProcessManager.py
+++ lib/JumpScale/tools/cuisine/CuisineProcessManager.py
@@ -307,7 +307,6 @@ class CuisineTmuxec(ProcessManagerBase):
             tmux detach -s {session}
             """
             start_command = command_template.format(session='main', window=name, command=cmd)
-            import ipdb; ipdb.set_trace()
             self._cuisine.core.file_ensure(self.startupfile)
             content = self._cuisine.core.file_read(self.startupfile)
             if "tmux send-keys '{command}' c-m".format(command=cmd) not in content:

942e9bfddbb278f341b5e8915cc4365082672b6e
christophe de Carvalho Pereira Martins <christophe@greenitglobe.com>
2017-04-19 11:21:21 +0400

    g8os sal: supports initProcess in container

--- lib/JumpScale/sal/g8os/Container.py
+++ lib/JumpScale/sal/g8os/Container.py
@@ -1,7 +1,8 @@
 class Container:
     """G8SO Container"""
 
-    def __init__(self, name, node, flist, hostname=None, filesystems=None, nics=None, host_network=False, ports=None, storage=None):
+
+    def __init__(self, name, node, flist, hostname=None, filesystems=None, nics=None, host_network=False, ports=None, storage=None, init_processes=None):
         """
         TODO: write doc string
         filesystems: dict {filesystemObj: target}
@@ -15,6 +16,7 @@ class Container:
         self.nics = nics or []
         self.host_network = host_network
         self.storage = storage
+        self.init_processes = init_processes or []
         self.id = None
         self._client = None
 
@@ -30,16 +32,27 @@ class Container:
             ports[int(source)] = int(dest)
         nics = [nic.to_dict() for nic in service.model.data.nics]
 
+        filesystems = {}
+        for mount in service.model.data.mounts:
+            fs_service = service.aysrepo.serviceGet('filesystem', mount.filesystem)
+            try:
+                sp = node.storagepools.get(fs_service.parent.name)
+                fs = sp.get(fs_service.name)
+            except KeyError:
+                continue
+            filesystems[fs] = mount.target
+
         container = cls(
             name=service.name,
             node=node,
-            # filesystems = service.model.data. TODO
+            filesystems=filesystems,
             nics=nics,
             hostname=service.model.data.hostname,
             flist=service.model.data.flist,
             ports=ports,
             host_network=service.model.data.hostNetworking,
-            storage=service.model.data.storage
+            storage=service.model.data.storage,
+            init_processes=[p.to_dict() for p in service.model.data.initProcesses],
         )
         if service.model.data.id != 0:
             container.id = service.model.data.id
@@ -71,6 +84,16 @@ class Container:
     def start(self):
         if not self.is_running():
             self._create_container()
+
+            for process in self.init_processes:
+                cmd = "{} {}".format(process['name'], ' '.join(process.get('args', [])))
+                pwd = process.get('pwd', '')
+                stdin = process.get('stdin', '')
+                env = {}
+                for x in process.get('environment', []):
+                    k, v = x.split("=")
+                    env[k] = v
+                self.client.system(command=cmd, dir=pwd, stdin=stdin, env=env)
 
     def stop(self):
         if not self.is_running():
